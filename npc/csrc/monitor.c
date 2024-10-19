4001fe5ac2152029753692c7d6d9b57c73b197fc
tracer-ysyx <tracer@ysyx.org>
2024-10-19 11:39:29 +0800

    > sim RTL 2023010201031 江贤 Linux jx-virtual-machine 6.5.0-18-generic #18~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb  7 11:40:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:39:29 up 48 min,  1 user,  load average: 0.93, 0.61, 0.48

--- npc/csrc/monitor.c
+++ npc/csrc/monitor.c
@@ -93,7 +93,7 @@ void init_monitor(int argc, char** argv) {
 
   /* Parse arguments. */
   parse_args(argc, argv);
-  sdb_set_batch_mode();
+  //sdb_set_batch_mode();
   //parse_elf(elf_file);
   /* Set random seen. */
   init_rand();
