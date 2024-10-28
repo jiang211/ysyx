53ee5e7928290e0e29dbc4dfd00193e7ffad975d
tracer-ysyx <tracer@ysyx.org>
2024-10-28 17:58:44 +0800

    > compile NEMU 2023010201031 江贤 Linux jx-virtual-machine 6.5.0-18-generic #18~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb  7 11:40:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:58:44 up  1:30,  1 user,  load average: 0.72, 0.54, 0.47

--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -20,9 +20,9 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
    * Then return the address of the interrupt/exception vector.
    */
 #ifdef CONFIG_ETRACE
-  printf(" mcause  =   0x%x \n", c->mcause );
-  printf(" mstatus = 0x%x \n", c->mstatus);
-  printf(" mepc    = 0x%x \n", c->mepc   );
+  printf(" mcause  =   0x%x \n", cpu.csr[2] );
+  printf(" mstatus = 0x%x \n", cpu.csr[3]);
+  printf(" mepc    = 0x%x \n", cpu.csr[0]   );
 #endif
   cpu.csr[1] = 0x1800;
   cpu.csr[0] = epc;
