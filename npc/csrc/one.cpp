a8c60c458b0f513f09cb8bcc2b967fab321d2ce3
tracer-ysyx <tracer@ysyx.org>
2024-10-28 16:53:11 +0800

    > sim RTL 2023010201031 江贤 Linux jx-virtual-machine 6.5.0-18-generic #18~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb  7 11:40:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:53:11 up 25 min,  1 user,  load average: 0.50, 0.48, 0.41

--- npc/csrc/one.cpp
+++ npc/csrc/one.cpp
@@ -146,14 +146,14 @@ void run_step(Decode *s, CPU_state *cpu, bool *vpmem_read_called ) {
        
         s->dnpc = top->dnpc;
         s->pc = top->pc;
-       printf("s->dnpc = %08x\n",top->dnpc);
-        printf("s->pc = %08x\n",top->pc);
+       
         s->isa.inst.val = top->instr;
         for (int i=0; i<32; i++) {
           cpu->gpr[i] = cpu_gpr[i];
         }
 
-      
+      printf("s->dnpc = %08x\n",top->dnpc);
+        printf("s->pc = %08x\n",top->pc);
       if(top->ebreak)  { 
         npc_trap(NPC_END , top->dnpc, cpu_gpr[10]);
         return ;
