e77f33aa50b7fbf5e68c19a8beaaf82b0b9f006f
tracer-ysyx <tracer@ysyx.org>
2024-12-04 21:37:08 +0800

    > sim RTL 2023010201031 江贤 Linux jx-virtual-machine 6.5.0-18-generic #18~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb  7 11:40:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:37:08 up 34 min,  4 users,  load average: 0.76, 0.55, 0.40

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -132,7 +132,7 @@ alu my_alu(
     .rs1_data       (rs1_data   ),
     .rs2_data       (rs2_data   ),
     .imm_data       (imm        ),
-    .pc_data        (inst_addr_o),
+    .pc_data        (inst_addr_o + 4),
     .alu_src1       (alu_src1   ),
     .alu_src2       (alu_src2   ),
     .branch         (branch     ),   
