c642d7f459820146a24990707040cb3ae8e6ebca
tracer-ysyx <tracer@ysyx.org>
2024-12-06 19:57:40 +0800

    > sim RTL 2023010201031 江贤 Linux jx-virtual-machine 6.5.0-18-generic #18~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb  7 11:40:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:57:40 up 11 min,  1 user,  load average: 0.35, 0.48, 0.44

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -191,7 +191,7 @@ assign clk_neg = ~clk & clk_reg;
 
 
 always @(*) begin
-    vpmem_read(raddr,{7'b0, ren},rdata_in);
+    vpmem_read(raddr,{7'b0, ren&clk},rdata_in);
 end
 
 always @(posedge clk_neg) begin
