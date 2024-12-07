aabc19320455859da797ef32f70e4ef354972ab8
tracer-ysyx <tracer@ysyx.org>
2024-12-07 23:06:00 +0800

    > sim RTL 2023010201031 江贤 Linux jx-virtual-machine 6.5.0-18-generic #18~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb  7 11:40:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:06:00 up  1:53,  1 user,  load average: 0.51, 0.43, 0.44

--- npc/vsrc/alu.v
+++ npc/vsrc/alu.v
@@ -29,15 +29,13 @@ module alu(
     wire [63:0] m_result = (u_alu_type) ? mulhu_result : mulh_result;
     assign opdata1 = (u_alu_type)? unsigned_a : signed_a;
     assign opdata2 = (u_alu_type)? unsigned_b : signed_b;
-    /*
+    
     always @(*) begin
     $display("Value of signal opdata1 is %08x", opdata1);
     $display("Value of signal opdata2 is %08x", opdata2&32'h0000001f);
     $display("Value of signal alu_out is %08x", alu_out);
-    $display("Value of signal zero is %08x", zero);
-    $display("Value of signal alu_crtl is %08x", alu_crtl);
     end
-    */
+    
     always @(*) begin
         case (alu_crtl)
             4'b0000: begin
