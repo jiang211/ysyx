f32028b6e361f3f8a21211c0d4a44dd788206964
tracer-ysyx <tracer@ysyx.org>
2024-12-08 16:33:41 +0800

    > sim RTL 2023010201031 江贤 Linux jx-virtual-machine 6.5.0-18-generic #18~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb  7 11:40:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:33:41 up 13 min,  1 user,  load average: 0.84, 0.57, 0.35

--- npc/csrc/one.cpp
+++ npc/csrc/one.cpp
@@ -86,7 +86,7 @@ extern "C" void vpmem_write(int waddr, char wlen,int wdata,char wen) {
 }
 
 extern "C" void vpmem_read(int raddr,char ren, int *rdata) {
-  if(ren && raddr>=0x80000000 && raddr <= 0x8fffffff){
+  if(ren && raddr>=0x80000000 && raddr <= 0x88000000){
     
       *rdata = paddr_read((paddr_t)(raddr),4);
     
