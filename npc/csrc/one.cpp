2fe17ed957e3e6bbb578754a8ddce484a1b29fdd
tracer-ysyx <tracer@ysyx.org>
2024-12-09 16:25:55 +0800

    > sim RTL 2023010201031 江贤 Linux jx-virtual-machine 6.5.0-18-generic #18~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb  7 11:40:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:25:55 up 25 min,  1 user,  load average: 0.82, 0.67, 0.55

--- npc/csrc/one.cpp
+++ npc/csrc/one.cpp
@@ -74,7 +74,7 @@ extern "C" void vpmem_write(int waddr, char wlen,int wdata,char wen) {
   // 总是往地址为`waddr & ~0x3u`的4字节按写掩码`wmask`写入`wdata`
   // `wmask`中每比特表示`wdata`中1个字节的掩码,
   // 如`wmask = 0x3`代表只写入最低2个字节, 内存中的其它字节保持不变
-  int count = 0;
+  
   if(wen){
       if(waddr == SERIAL_ADDR){
         putchar(wdata);
