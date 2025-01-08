5bc9f7765d77778cb4856581ae63428c5fc15b69
tracer-ysyx <tracer@ysyx.org>
2025-01-08 18:52:58 +0800

    > compile NEMU 2023010201031 江贤 Linux jx-virtual-machine 6.5.0-18-generic #18~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb  7 11:40:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:52:58 up 13 min,  1 user,  load average: 1.01, 1.02, 0.62

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -58,7 +58,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
            memset((void*)(phdr[i].p_vaddr+phdr[i].p_filesz), 0, phdr[i].p_memsz - phdr[i].p_filesz);
        }
    }
-   printf("jump to entry = %p\n", (void*)ehdr.e_entry);
+   printf("jump to entry = %08x\n", ehdr.e_entry);
    return ehdr.e_entry;
 
 }
