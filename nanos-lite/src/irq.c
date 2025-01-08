02179a381d78346f4093e88c9f59db37406f0ba4
tracer-ysyx <tracer@ysyx.org>
2025-01-08 18:55:37 +0800

    > compile NEMU 2023010201031 江贤 Linux jx-virtual-machine 6.5.0-18-generic #18~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb  7 11:40:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:55:37 up 16 min,  1 user,  load average: 1.70, 1.26, 0.78

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -1,6 +1,7 @@
 #include <common.h>
 #define EVENT_YIELD 1
 static Context* do_event(Event e, Context* c) {
+  printf("Received event %d\n", e.event);
   switch (e.event) {
     case EVENT_YIELD : 
       printf("Received event 1\n");
