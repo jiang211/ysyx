4c5bd06eb418e0d39953cd1b1d5cdc252ca64a69
tracer-ysyx <tracer@ysyx.org>
2025-01-08 19:55:59 +0800

    > compile NEMU 2023010201031 江贤 Linux jx-virtual-machine 6.5.0-18-generic #18~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb  7 11:40:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:55:59 up  1:16,  1 user,  load average: 0.29, 0.39, 0.51

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -2,7 +2,7 @@
 #define EVENT_YIELD 1
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
-    case EVENT_YIELD: printf("event 's ID = 1\n",e.event);c->mepc += 4;break;
+    case EVENT_YIELD: printf("event 's ID = 1\n",e.event);break;
     default: panic("Unhandled event ID = %d", e.event);
   }
 
