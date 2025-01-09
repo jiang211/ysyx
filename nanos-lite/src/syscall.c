c446c874064ff8a861465fde3d227601af0aa397
tracer-ysyx <tracer@ysyx.org>
2025-01-09 14:27:23 +0800

    > compile NEMU 2023010201031 江贤 Linux jx-virtual-machine 6.5.0-18-generic #18~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb  7 11:40:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:27:23 up 35 min,  1 user,  load average: 0.35, 0.31, 0.24

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -4,12 +4,12 @@
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
-  printf(" syscall_ID  =%d0\n", a[0] );
+  printf(" syscall_ID  =%d0\n", c->GPR1 );
   #ifdef CONFIG_STRACE
   printf(" syscall_ID  =   0x%x \n", a[0] );
 #endif
   switch (a[0]) {
-    case 0:printf("1\n");halt(c->GPRx);break;
+    case 0:halt(c->GPRx);break;
     case 1:yield();break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
