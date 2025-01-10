72adb3fa203f52db5aeaca4f3cc4778209e4f2ac
tracer-ysyx <tracer@ysyx.org>
2025-01-10 20:50:30 +0800

    > compile NEMU 2023010201031 江贤 Linux jx-virtual-machine 6.5.0-18-generic #18~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb  7 11:40:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:50:30 up  1:02,  1 user,  load average: 0.80, 0.50, 0.39

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -28,7 +28,7 @@ void do_syscall(Context *c) {
     case SYS_exit:halt(c->GPR1);break;
     case SYS_yield:yield();c->GPRx = 0;break;
     case SYS_write:sys_write(c);break;
-    case SYS_brk :c->GPRx = 0;break;
+    //case SYS_brk :c->GPRx = 0;break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 }
