93d4b3433651e88a92d3eea6effc8996018c111b
tracer-ysyx <tracer@ysyx.org>
2025-01-11 21:19:32 +0800

    > compile NEMU 2023010201031 江贤 Linux jx-virtual-machine 6.5.0-18-generic #18~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb  7 11:40:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:19:32 up  2:11,  1 user,  load average: 0.50, 0.48, 0.44

--- navy-apps/tests/timer_test/timer_test.c
+++ navy-apps/tests/timer_test/timer_test.c
@@ -6,7 +6,7 @@ int main() {
   struct timeval tv;
   // struct timezone tz;
   gettimeofday(&tv, NULL);
-  __uint64_t ms = 500;
+  int ms = 500;
   while (1) {
     while ((tv.tv_sec * 1000 + tv.tv_usec / 1000) < ms) {
       gettimeofday(&tv, NULL);
