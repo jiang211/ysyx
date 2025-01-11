00bd5c3a737358a3447b61b81def54458c1ab3a6
tracer-ysyx <tracer@ysyx.org>
2025-01-11 21:03:57 +0800

    > compile NEMU 2023010201031 江贤 Linux jx-virtual-machine 6.5.0-18-generic #18~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb  7 11:40:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:03:57 up  1:55,  1 user,  load average: 0.58, 0.55, 0.46

--- navy-apps/tests/timer_test/timer_test.c
+++ navy-apps/tests/timer_test/timer_test.c
@@ -3,17 +3,24 @@
 #include <unistd.h>
 
 int main() {
-    struct timeval tv;
-    struct timezone tz;
-    int count = 0;
-    while (1) {
-        gettimeofday(&tv, NULL);
-        while(tv.tv_usec/1000 < count);
-        
+    struct timeval tv, tv_start, tv_end;
+    long long elapsed;  // 用于存储已经过去的时间（微秒）
 
+    while (1) {
+        // 获取当前时间作为开始时间
+        gettimeofday(&tv_start, NULL);
+
+        // 打印当前时间
         printf("Current time: %ld seconds and %ld microseconds since the Epoch\n",
-               tv.tv_sec, tv.tv_usec);
-        count++;
+               tv_start.tv_sec, tv_start.tv_usec);
+
+        // 忙等待直到0.5秒过去
+        do {
+            gettimeofday(&tv_end, NULL);
+            elapsed = (tv_end.tv_sec - tv_start.tv_sec) * 1000000LL + (tv_end.tv_usec - tv_start.tv_usec);
+        } while (elapsed < 500000);
+
+        // 此时已经过去了大约0.5秒
     }
 
     return 0;
