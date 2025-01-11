f236d127039b0521b63e6796e706fdb770e9b714
tracer-ysyx <tracer@ysyx.org>
2025-01-11 16:06:57 +0800

    > compile NEMU 2023010201031 江贤 Linux jx-virtual-machine 6.5.0-18-generic #18~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb  7 11:40:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:06:57 up  2:03,  1 user,  load average: 0.99, 0.70, 0.54

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -20,11 +20,11 @@ void do_syscall(Context *c) {
   switch (a[0]) {
     case SYS_exit:halt(c->GPR1);break;
     case SYS_yield:yield();c->GPRx = 0;break;
-    case SYS_open:fs_open((char *)a[1], (int)a[2], (int)a[3]);break;
-    case SYS_close:fs_close((int)a[1]);break;
-    case SYS_lseek:fs_lseek((int)a[1], (size_t)a[2], (int)a[3]);break;
-    case SYS_read:fs_read((int)a[1], (void *)a[2], (size_t)a[3]);break;
-    case SYS_write:fs_write((int)a[1], (void *)a[2], (size_t)a[3]);break;
+    case SYS_open:c->GPRx = fs_open((char *)a[1], (int)a[2], (int)a[3]);break;
+    case SYS_close:c->GPRx = fs_close((int)a[1]);break;
+    case SYS_lseek:c->GPRx = fs_lseek((int)a[1], (size_t)a[2], (int)a[3]);break;
+    case SYS_read:c->GPRx = fs_read((int)a[1], (void *)a[2], (size_t)a[3]);break;
+    case SYS_write:c->GPRx = fs_write((int)a[1], (void *)a[2], (size_t)a[3]);break;
     case SYS_brk :c->GPRx = 0;break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
