fa55c967e54403c88403db6c5b2cd54a7d77ac6d
tracer-ysyx <tracer@ysyx.org>
2025-01-12 14:50:18 +0800

    > compile NEMU 2023010201031 江贤 Linux jx-virtual-machine 6.5.0-18-generic #18~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb  7 11:40:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:50:18 up 49 min,  1 user,  load average: 0.75, 0.59, 0.47

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -22,8 +22,17 @@ size_t serial_write(const void *buf, size_t offset, size_t len) {
 }
 
 size_t events_read(void *buf, size_t offset, size_t len) {
-  return 0;
+  AM_INPUT_KEYBRD_T ev = io_read(AM_INPUT_KEYBRD);
+  size_t ret = 0 ;
+  if( ev.keycode == AM_KEY_NONE )  {
+    ret =  0;
+  }
+  else { 
+    ret = snprintf(buf, len, "%s %s\n", ev.keydown?"kd":"ku", keyname[ev.keycode]);
+  }
+  return ret;
 }
+
 
 size_t dispinfo_read(void *buf, size_t offset, size_t len) {
   return 0;
--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -22,7 +22,12 @@ uint32_t NDL_GetTicks() {
 }
 
 int NDL_PollEvent(char *buf, int len) {
-  return 0;
+  int fd = open("/dev/events",0,0);
+  int ret = read(fd,buf,len);
+  if(ret == 0){
+    return 0;
+  }
+  return 1;
 }
 
 void NDL_OpenCanvas(int *w, int *h) {
