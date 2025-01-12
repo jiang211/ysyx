75112335f16c4066d613b9a475aa02d4a567fb1b
tracer-ysyx <tracer@ysyx.org>
2025-01-12 16:25:39 +0800

    > compile NEMU 2023010201031 江贤 Linux jx-virtual-machine 6.5.0-18-generic #18~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb  7 11:40:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:25:39 up  2:24,  1 user,  load average: 1.08, 0.78, 0.58

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -94,7 +94,7 @@ int NDL_Init(uint32_t flags) {
     }
   }
   *width_p = '\0';
-  screen_w = atoi(WIDTH);
+  //screen_w = atoi(WIDTH);
   printf("init : screen_w = %d\n", screen_w);
   for( ; (i < sizeof(buf)) && (*(buf+i)!='\0') ; i++) {
     if( *(buf+i) >= '0' && *(buf+i) <= '9' ){
@@ -103,7 +103,7 @@ int NDL_Init(uint32_t flags) {
     }
   }
   *height_p = '\0';
-  screen_h = atoi(HEIGHT);
+  //screen_h = atoi(HEIGHT);
   printf("init : screen_h = %d\n", screen_h);
 
   if (getenv("NWM_APP")) {
