78a4c56f957df96065162fe25e6961dfae87cac7
tracer-ysyx <tracer@ysyx.org>
2025-01-12 15:03:51 +0800

    > compile NEMU 2023010201031 江贤 Linux jx-virtual-machine 6.5.0-18-generic #18~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb  7 11:40:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:03:51 up  1:02,  1 user,  load average: 0.90, 0.54, 0.48

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -4,6 +4,11 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/time.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
 static int evtdev = -1;
 static int fbdev = -1;
 static int screen_w = 0, screen_h = 0;
@@ -23,9 +28,8 @@ uint32_t NDL_GetTicks() {
 
 int NDL_PollEvent(char *buf, int len) {
   int fd = open("/dev/events",0,0);
-  int ret = read(fd,buf,len);
   
-  return ret;
+  return read(fd,buf,len);
 }
 
 void NDL_OpenCanvas(int *w, int *h) {
