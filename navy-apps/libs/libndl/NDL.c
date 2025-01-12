e26660ab23381314982b1a50b1cf7e27ea828d0c
tracer-ysyx <tracer@ysyx.org>
2025-01-12 21:16:10 +0800

    > compile NEMU 2023010201031 江贤 Linux jx-virtual-machine 6.5.0-18-generic #18~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb  7 11:40:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:16:10 up 48 min,  1 user,  load average: 0.60, 0.44, 0.43

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -35,6 +35,11 @@ int NDL_PollEvent(char *buf, int len) {
 }
 
 void NDL_OpenCanvas(int *w, int *h) {
+  int vga_fd = open("/proc/dispinfo", 0,0);
+  char buf[64];
+  read(vga_fd, buf, sizeof(buf));
+  sscanf(buf, "WIDTH:%d\nHEIGHT:%d\n", &screen_w, &screen_h);
+  printf("screen_w:%d, screen_h:%d\n", screen_w, screen_h);
   assert( screen_w != 0 && screen_h != 0 );
   if((*w == 0) && (*h == 0)){
     *w = screen_w;
@@ -62,6 +67,47 @@ void NDL_OpenCanvas(int *w, int *h) {
 }
 
 void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
+  int draw_x;
+  int draw_y;
+  int draw_w;
+  int draw_h;
+  int fbfd = open("/dev/fb", 0, 0);
+  if( w == 0 ) w = canvas_w;
+  if( h == 0 ) h = canvas_h;
+
+  if( x < 0 ) {
+    draw_x = 0;
+    draw_w = w+x;
+  }
+  else {
+    draw_x = x ;
+    draw_w = w ;
+  }
+  int max_w = canvas_w - draw_x;
+  if( max_w < draw_w )   draw_w = max_w;
+
+  if( y < 0 ) {
+    draw_y = 0 ;
+    draw_h = h+y ;
+  }
+  else {
+    draw_y = y ;
+    draw_h = h ;
+  }
+  int max_h = canvas_h - draw_y;
+  if( max_h < draw_h ) draw_h = max_h;
+  if(draw_w <=0 || draw_h <=0 )
+    return ;
+
+  int i  ;
+  uint8_t *ret = (uint8_t *)pixels;
+
+    for( int i = 0; i < h; i++ ) {
+        lseek(fbfd, (((i+draw_y+canvas_y)*screen_w + draw_x+canvas_x)*4), SEEK_SET);
+        write(fbfd, ret , draw_w*4);
+        ret = ret + draw_w*4 ;
+    }
+
 }
 
 void NDL_OpenAudio(int freq, int channels, int samples) {
@@ -81,11 +127,11 @@ int NDL_QueryAudio() {
 
 int NDL_Init(uint32_t flags) {
   // 打开vga的dispinfo文件并解析出屏幕大小
-  int vga_fd = open("/proc/dispinfo", 0,0);
-  char buf[64];
-  read(vga_fd, buf, sizeof(buf));
-  sscanf(buf, "WIDTH:%d\nHEIGHT:%d\n", &screen_w, &screen_h);
-  printf("screen_w:%d, screen_h:%d\n", screen_w, screen_h);
+  
+
+  // 打开/dev/fb0并设置屏幕大小
+  
+
 
   if (getenv("NWM_APP")) {
     evtdev = 3;
