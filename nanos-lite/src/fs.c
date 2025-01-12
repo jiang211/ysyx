430559169f7250d722baab1b69faabded052645a
tracer-ysyx <tracer@ysyx.org>
2025-01-12 15:13:29 +0800

    > compile NEMU 2023010201031 江贤 Linux jx-virtual-machine 6.5.0-18-generic #18~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb  7 11:40:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:13:29 up  1:12,  1 user,  load average: 0.40, 0.63, 0.59

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -44,7 +44,6 @@ int fs_open(const char *pathname, int flags, int mode){
   int len = sizeof(file_table)/sizeof(Finfo);
   for(int i=0; i<len; i++){
     if(strcmp(file_table[i].name, pathname) == 0){
-      printf("file %s found\n", pathname);
       file_table[i].open_offset = 0;
       return i;
     }
@@ -56,6 +55,9 @@ int fs_open(const char *pathname, int flags, int mode){
 size_t fs_read(int fd, void *buf, size_t len){
   if (fd < 3) {
     return 0;
+  }
+  if(file_table[fd].read != NULL) {
+    return file_table[fd].read(buf, 0, len);
   }
   size_t size = file_table[fd].size;
   size_t offset = file_table[fd].open_offset;
