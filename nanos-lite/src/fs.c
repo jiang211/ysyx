d5a39552c17a1b395facc2eaf5d1db4ac0dbc1da
tracer-ysyx <tracer@ysyx.org>
2025-01-12 16:37:57 +0800

    > compile NEMU 2023010201031 江贤 Linux jx-virtual-machine 6.5.0-18-generic #18~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb  7 11:40:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:37:57 up  2:36,  1 user,  load average: 0.74, 0.65, 0.60

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -18,7 +18,7 @@ typedef struct {
   size_t open_offset;
 } Finfo;
 
-enum {FD_STDIN, FD_STDOUT, FD_STDERR, FD_EVENTS,FD_FB,FD_DISPINFO};
+enum {FD_STDIN, FD_STDOUT, FD_STDERR, FD_EVENTS,FD_DISPINFO,FD_FB};
 
 size_t invalid_read(void *buf, size_t offset, size_t len) {
   panic("should not reach here");
