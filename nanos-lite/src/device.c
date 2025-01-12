11008649080d19fbf25b4bcf37662fbd8488b3a2
tracer-ysyx <tracer@ysyx.org>
2025-01-12 16:38:31 +0800

    > compile NEMU 2023010201031 江贤 Linux jx-virtual-machine 6.5.0-18-generic #18~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb  7 11:40:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:38:31 up  2:37,  1 user,  load average: 0.86, 0.67, 0.61

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -37,6 +37,7 @@ size_t events_read(void *buf, size_t offset, size_t len) {
 size_t dispinfo_read(void *buf, size_t offset, size_t len) {
   AM_GPU_CONFIG_T config = io_read(AM_GPU_CONFIG);
   snprintf(buf, len, "width=%d, height=%d\n", config.width, config.height);
+  printf("1\n");
   return 0;
 }
 
