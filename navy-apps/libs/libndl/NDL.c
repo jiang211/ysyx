1b2a426ad35594407d398cd1243d4b939b8b8ff4
tracer-ysyx <tracer@ysyx.org>
2025-01-12 16:26:47 +0800

    > compile NEMU 2023010201031 江贤 Linux jx-virtual-machine 6.5.0-18-generic #18~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb  7 11:40:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:26:47 up  2:25,  1 user,  load average: 0.87, 0.77, 0.59

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -40,8 +40,6 @@ void NDL_OpenCanvas(int *w, int *h) {
   }
   canvas_w = *w ; 
   canvas_h = *h ; 
-  canvas_x = (screen_w -canvas_w) / 2 ;
-  canvas_y = (screen_h -canvas_h) / 2 ;
   if (getenv("NWM_APP")) {
     int fbctl = 4;
     fbdev = 5;
