3331f8d7907d5e9fca3b763271762ce2deab2d9a
tracer-ysyx <tracer@ysyx.org>
2025-01-12 15:05:00 +0800

    > compile NEMU 2023010201031 江贤 Linux jx-virtual-machine 6.5.0-18-generic #18~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb  7 11:40:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:05:00 up  1:04,  1 user,  load average: 1.12, 0.70, 0.55

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -28,6 +28,7 @@ size_t events_read(void *buf, size_t offset, size_t len) {
     ret =  0;
   }
   else { 
+    printf("1\n");
     ret = snprintf(buf, len, "%s %s\n", ev.keydown?"kd":"ku", keyname[ev.keycode]);
   }
   return ret;
