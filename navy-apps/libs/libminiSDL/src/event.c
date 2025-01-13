b72eae748bbdbf2b4e32116f1ec71ee0c690f265
tracer-ysyx <tracer@ysyx.org>
2025-01-13 14:44:43 +0800

    > compile NEMU 2023010201031 江贤 Linux jx-virtual-machine 6.5.0-18-generic #18~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb  7 11:40:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:44:43 up  1:01,  1 user,  load average: 0.59, 0.50, 0.45

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -1,6 +1,6 @@
 #include <NDL.h>
 #include <SDL.h>
-
+#include <string.h>
 #define keyname(k) #k,
 
 static const char *keyname[] = {
@@ -17,7 +17,18 @@ int SDL_PollEvent(SDL_Event *ev) {
 }
 
 int SDL_WaitEvent(SDL_Event *event) {
-  return 1;
+  char buf[20];
+  while (1) {
+    if (NDL_PollEvent(buf, sizeof(buf)) == 0) continue;
+    event->type = buf[1] == 'u' ? SDL_KEYUP : SDL_KEYDOWN;
+    for (int i = 0; i < 83; i++) {
+      if (strcmp(keyname[i], buf + 3) == 0) {
+        event->key.keysym.sym = i;
+        return 1;
+      }
+    }
+  }
+  return 0;
 }
 
 int SDL_PeepEvents(SDL_Event *ev, int numevents, int action, uint32_t mask) {
