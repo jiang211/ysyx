f17167ee87499882708f6632775f02ab4eae9fd7
tracer-ysyx <tracer@ysyx.org>
2025-01-13 20:39:51 +0800

    > compile NEMU 2023010201031 江贤 Linux jx-virtual-machine 6.5.0-18-generic #18~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb  7 11:40:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:39:51 up  2:05,  1 user,  load average: 1.17, 0.60, 0.49

--- npc/csrc/reg.c
+++ npc/csrc/reg.c
@@ -9,7 +9,7 @@ const char *regs[] = {
 };
 
 #define gpr(idx) (cpu.gpr[check_reg_idx(idx)])
-
+#define csr(idx) (cpu.csr[idx])
 word_t isa_reg_str2val(const char *s, bool *success) {
     
     for(int i = 0 ; i < 32 ; i ++){
