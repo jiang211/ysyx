f3a6885a477bd2e8f9026ce2c596b7fa3caf02bc
tracer-ysyx <tracer@ysyx.org>
2025-01-13 15:12:55 +0800

    > compile NEMU 2023010201031 江贤 Linux jx-virtual-machine 6.5.0-18-generic #18~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb  7 11:40:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:12:55 up  1:29,  1 user,  load average: 0.65, 0.66, 0.60

--- navy-apps/libs/libminiSDL/src/timer.c
+++ navy-apps/libs/libminiSDL/src/timer.c
@@ -2,7 +2,7 @@
 #include <sdl-timer.h>
 #include <stdio.h>
 #include <sys/time.h>
-uint32_t init_time = 0 ;
+uint32_t init_time1 = 0 ;
 SDL_TimerID SDL_AddTimer(uint32_t interval, SDL_NewTimerCallback callback, void *param) {
   return NULL;
 }
@@ -14,11 +14,11 @@ int SDL_RemoveTimer(SDL_TimerID id) {
 uint32_t SDL_GetTicks() {
   struct timeval tv;
 	gettimeofday(&tv,NULL);
-  if( init_time == 0 ) {
-    init_time = tv.tv_sec * 1000 + tv.tv_usec/1000;
+  if( init_time1 == 0 ) {
+    init_time1 = tv.tv_sec * 1000 + tv.tv_usec/1000;
   }
   uint32_t now = tv.tv_sec * 1000 + tv.tv_usec/1000;
-  uint32_t total_time = (now - init_time);
+  uint32_t total_time = (now - init_time1);
   return total_time;
 }
 
