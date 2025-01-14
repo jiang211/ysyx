120503c496eacb24ea866e611f087f4fa8c971d4
tracer-ysyx <tracer@ysyx.org>
2025-01-14 20:24:29 +0800

    > compile NEMU 2023010201031 江贤 Linux jx-virtual-machine 6.5.0-18-generic #18~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb  7 11:40:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:24:29 up  2:33,  1 user,  load average: 0.42, 0.56, 0.56

--- npc/csrc/one.cpp
+++ npc/csrc/one.cpp
@@ -106,7 +106,6 @@ extern "C" void vpmem_read(int raddr,char ren, int *rdata) {
   }
   else if(ren && raddr == RTC_ADDR1){
       uint64_t us = get_time();
-      printf("%ld\n",us);
       *rdata = (uint32_t)us;
     #ifdef MTRACE
       printf("addr = %08x , rdata = %08x\n",raddr,*rdata);
