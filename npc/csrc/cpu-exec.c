f064b1a36b79b2fde87ea69ee7e6eeb85e651278
tracer-ysyx <tracer@ysyx.org>
2025-01-14 20:29:28 +0800

    > sim RTL 2023010201031 江贤 Linux jx-virtual-machine 6.5.0-18-generic #18~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb  7 11:40:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:29:28 up  2:38,  1 user,  load average: 0.84, 0.80, 0.68

--- npc/csrc/cpu-exec.c
+++ npc/csrc/cpu-exec.c
@@ -148,7 +148,6 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc){
   if( g_print_step ) { puts(_this->logbuf); }
 
 #ifdef CONFIG_DIFFTEST
- printf("1\n");
     difftest_step(_this->pc, dnpc);
 #endif
 }
