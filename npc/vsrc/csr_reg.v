59df704fe701d8dc27b4a8070cb7bd341daf7694
tracer-ysyx <tracer@ysyx.org>
2025-01-15 21:08:43 +0800

    > sim RTL 2023010201031 江贤 Linux jx-virtual-machine 6.5.0-18-generic #18~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb  7 11:40:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:08:43 up  2:01,  1 user,  load average: 0.67, 0.53, 0.49

--- npc/vsrc/csr_reg.v
+++ npc/vsrc/csr_reg.v
@@ -24,7 +24,7 @@ module csr_reg #(
     assign clk_neg = ~clk & clk_reg;
     import "DPI-C" function void set_csr_ptr(input logic [31:0] a []);
     initial set_csr_ptr(csr); // set the pointer to the CSR array
-    
+    initial csr[1] = 'h1800;
     always @(posedge clk_neg) begin
         if (ecall) begin 
             csr[2] <= 'd11; 
