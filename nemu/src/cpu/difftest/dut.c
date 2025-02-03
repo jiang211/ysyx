0a81b512c6787647d47e8a7589caa2f1551529dc
tracer-ysyx <tracer@ysyx.org>
2025-02-03 23:21:34 +0800

    > compile NEMU 2023010201031 江贤 Linux jx-virtual-machine 6.5.0-18-generic #18~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb  7 11:40:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:21:34 up 43 min,  1 user,  load average: 1.06, 0.79, 0.59

--- nemu/src/cpu/difftest/dut.c
+++ nemu/src/cpu/difftest/dut.c
@@ -121,7 +121,7 @@ void difftest_step(vaddr_t pc, vaddr_t npc) {
     is_skip_ref = false;
     return;
   }
-
+  printf("1\n");
   ref_difftest_exec(1);
   ref_difftest_regcpy(&ref_r, DIFFTEST_TO_DUT);
 
