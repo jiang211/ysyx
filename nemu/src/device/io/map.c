a11223fad217961043984f0bbce18459c7dac0ec
tracer-ysyx <tracer@ysyx.org>
2025-02-06 15:29:24 +0800

    > compile NEMU 2023010201031 江贤 Linux jx-virtual-machine 6.5.0-18-generic #18~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb  7 11:40:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:29:24 up 54 min,  1 user,  load average: 1.21, 0.75, 0.66

--- nemu/src/device/io/map.c
+++ nemu/src/device/io/map.c
@@ -33,6 +33,7 @@ uint8_t* new_space(int size) {
 }
 
 static void check_bound(IOMap *map, paddr_t addr) {
+  printf("map = %s\n",map->name);
   if (map == NULL) {
     Assert(map != NULL, "address (" FMT_PADDR ") is out of bound at pc = " FMT_WORD, addr, cpu.pc);
   } else {
@@ -66,9 +67,7 @@ word_t map_read(paddr_t addr, int len, IOMap *map) {
 
 void map_write(paddr_t addr, int len, word_t data, IOMap *map) {
   assert(len >= 1 && len <= 8);
-  printf("map = %08x\n",addr);
   check_bound(map, addr);
-  printf("map = %08x\n",addr);
   #ifdef CONFIG_IDTRACE
   printf("device %s write at " FMT_PADDR " len=%d, data=" FMT_WORD "\n",map->name, addr, len, data);
   #endif
