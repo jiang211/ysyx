d2ce44ba2711f9adc4f7722532cd5c0fb146df2c
tracer-ysyx <tracer@ysyx.org>
2025-02-06 15:56:56 +0800

    > compile NEMU 2023010201031 江贤 Linux jx-virtual-machine 6.5.0-18-generic #18~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb  7 11:40:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:56:56 up  1:22,  1 user,  load average: 1.54, 0.74, 0.56

--- nemu/src/device/io/mmio.c
+++ nemu/src/device/io/mmio.c
@@ -59,6 +59,6 @@ word_t mmio_read(paddr_t addr, int len) {
 }
 
 void mmio_write(paddr_t addr, int len, word_t data) {
-  printf("map = %08x\n",addr);
+
   map_write(addr, len, data, fetch_mmio_map(addr));
 }
