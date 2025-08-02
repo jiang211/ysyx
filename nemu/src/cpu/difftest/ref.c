ccd300efc66fd0241d858a7d8a8b8a9341aec102
tracer-ysyx <tracer@ysyx.org>
2025-08-02 12:43:13 +0800

    > compile NEMU 2023010201031 江贤 Linux jx-virtual-machine 6.5.0-18-generic #18~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb  7 11:40:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:43:13 up 16 min,  1 user,  load average: 0.73, 0.56, 0.44

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -19,13 +19,14 @@
 #include <memory/paddr.h>
 
 __EXPORT void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction) {
+  printf("1\n");
   if(direction == DIFFTEST_TO_DUT){
     memcpy(addr - 0x80000000 + buf,guest_to_host(RESET_VECTOR),n);
   }
   else if(direction == DIFFTEST_TO_REF){
     memcpy(guest_to_host(RESET_VECTOR),addr - 0x80000000 + buf,n);
   }
-  
+  printf("2\n");
 }
 
 __EXPORT void difftest_regcpy(CPU_state *dut, bool direction) {
