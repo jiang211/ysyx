de431ae2b229e4ec5a5ad11bf89fcf9f1ed0cd49
tracer-ysyx <tracer@ysyx.org>
2025-08-02 11:57:16 +0800

    > compile NEMU 2023010201031 江贤 Linux jx-virtual-machine 6.5.0-18-generic #18~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb  7 11:40:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:57:16 up  2:44,  1 user,  load average: 1.09, 1.05, 0.98

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -118,7 +118,7 @@ void init_monitor(int argc, char *argv[]) {
 
   /* Initialize memory. */
   init_mem();
-printf("1");
+printf("1\n");
   /* Initialize devices. */
   IFDEF(CONFIG_DEVICE, init_device());
 printf("1");
