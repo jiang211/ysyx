c5b8f8a6023606621f0ad678a958ca728bcbaad7
tracer-ysyx <tracer@ysyx.org>
2025-08-05 19:20:47 +0800

    > compile NEMU 2023010201031 江贤 Linux jx-virtual-machine 6.5.0-18-generic #18~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb  7 11:40:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:20:47 up  5:23,  1 user,  load average: 1.15, 0.85, 0.67

--- nemu/include/cpu/ifetch.h
+++ nemu/include/cpu/ifetch.h
@@ -18,9 +18,7 @@
 #include <memory/vaddr.h>
 
 static inline uint32_t inst_fetch(vaddr_t *pc, int len) {
-  printf("inst_fetch: pc = %08x, len = %d\n", *pc, len);
   uint32_t inst = vaddr_ifetch(*pc, len);
-  printf("inst_fetch: inst = %08x\n", inst);
   (*pc) += len;
   return inst;
 }
