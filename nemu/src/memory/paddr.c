400d860d058247452a50566717748121c61e3e7a
tracer-ysyx <tracer@ysyx.org>
2025-08-05 19:15:45 +0800

    > compile NEMU 2023010201031 江贤 Linux jx-virtual-machine 6.5.0-18-generic #18~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb  7 11:40:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:15:45 up  5:18,  1 user,  load average: 0.56, 0.63, 0.57

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -29,8 +29,8 @@ static uint8_t pmem[CONFIG_MSIZE] PG_ALIGN = {};
 #define SRAM_size 0x01000000
 uint8_t* guest_to_host(paddr_t paddr) { return pmem + paddr - CONFIG_MBASE; }
 paddr_t host_to_guest(uint8_t *haddr) { return haddr - pmem + CONFIG_MBASE; }
-uint8_t* guest_to_host_mrom(paddr_t paddr) { return pmem + paddr - MROM_base; }
-uint8_t* guest_to_host_sram(paddr_t paddr) { return pmem + paddr - SRAM_base; }
+uint8_t* guest_to_host_mrom(paddr_t paddr) { return pmem + paddr; }
+uint8_t* guest_to_host_sram(paddr_t paddr) { return pmem + paddr ; }
 static word_t pmem_read(paddr_t addr, int len) {
   word_t ret = host_read(guest_to_host(addr), len);
   return ret;
