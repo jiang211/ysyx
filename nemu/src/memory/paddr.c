fa5ceaebf56a536de23a5ea27710d6103d07f874
tracer-ysyx <tracer@ysyx.org>
2025-08-10 14:04:23 +0800

    > compile NEMU 2023010201031 江贤 Linux jx-virtual-machine 6.5.0-18-generic #18~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb  7 11:40:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:04:23 up 5 min,  1 user,  load average: 0.56, 0.67, 0.36

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -18,62 +18,25 @@
 #include <device/mmio.h>
 #include <isa.h>
 
+
+
+#ifndef CONFIG_TARGET_SHARE
 #if   defined(CONFIG_PMEM_MALLOC)
 static uint8_t *pmem = NULL;
 #else // CONFIG_PMEM_GARRAY
 static uint8_t pmem[CONFIG_MSIZE] PG_ALIGN = {};
-static uint8_t psrammem[0x20000000] PG_ALIGN = {};
 #endif
-#define MROM_base 0x20000000
-#define MROM_size 0x00001000
-#define FLASH_base 0x30000000
-#define FLASH_size 0x10000000
-#define SRAM_base 0x0f000000
-#define SRAM_size 0x01000000
-#define PSRAM_base 0x80000000
-#define PSRAM_size 0x20000000
+
 uint8_t* guest_to_host(paddr_t paddr) { return pmem + paddr - CONFIG_MBASE; }
-uint8_t* guest_to_host_psram(paddr_t paddr) { return psrammem + paddr - PSRAM_base; }
 paddr_t host_to_guest(uint8_t *haddr) { return haddr - pmem + CONFIG_MBASE; }
-uint8_t* guest_to_host_mrom(paddr_t paddr) { return pmem + paddr - MROM_base; }
-uint8_t* guest_to_host_flash(paddr_t paddr) { return pmem + paddr - FLASH_base; }
-uint8_t* guest_to_host_sram(paddr_t paddr) { return pmem + paddr - SRAM_base + MROM_size; }
+
 static word_t pmem_read(paddr_t addr, int len) {
   word_t ret = host_read(guest_to_host(addr), len);
   return ret;
 }
 
-static word_t mrom_read(paddr_t addr, int len) {
-  word_t ret = host_read(guest_to_host_mrom(addr), len);
-  return ret;
-}
-
-static word_t flash_read(paddr_t addr, int len) {
-  word_t ret = host_read(guest_to_host_flash(addr), len);
-  return ret;
-}
-static word_t sram_read(paddr_t addr, int len) {
-  word_t ret = host_read(guest_to_host_sram(addr), len);
-  return ret;
-}
-
-static word_t psram_read(paddr_t addr, int len) {
-  word_t ret = host_read(guest_to_host_psram(addr), len);
-  return ret;
-}
-
 static void pmem_write(paddr_t addr, int len, word_t data) {
   host_write(guest_to_host(addr), len, data);
-}
-
-
-
-static void sram_write(paddr_t addr, int len, word_t data) {
-  host_write(guest_to_host_sram(addr), len, data);
-}
-
-static void psram_write(paddr_t addr, int len, word_t data) {
-  host_write(guest_to_host_psram(addr), len, data);
 }
 
 static void out_of_bound(paddr_t addr) {
@@ -86,34 +49,113 @@ void init_mem() {
   pmem = malloc(CONFIG_MSIZE);
   assert(pmem);
 #endif
-  IFDEF(CONFIG_MEM_RANDOM, memset(pmem, rand(), CONFIG_MSIZE));
+#ifdef CONFIG_MEM_RANDOM
+  uint32_t *p = (uint32_t *)pmem;
+  int i;
+  for (i = 0; i < (int) (CONFIG_MSIZE / sizeof(p[0])); i ++) {
+    p[i] = rand();
+  }
+#endif
   Log("physical memory area [" FMT_PADDR ", " FMT_PADDR "]", PMEM_LEFT, PMEM_RIGHT);
 }
 
 word_t paddr_read(paddr_t addr, int len) {
-#ifdef CONFIG_IMTRACE
-  printf("read at"FMT_PADDR "\n",addr);
+  if (likely(in_pmem(addr))) {
+    word_t ret = pmem_read(addr, len);
+#ifdef CONFIG_MTRACE
+    if(addr >= CONFIG_MTRACE_START && addr <= CONFIG_MTRACE_START + CONFIG_MTRACE_SIZE) {
+      Log(" MTRACE: Read  Memory Address: 0x%x, len: %d, data: 0x%x", addr, len, ret);
+    }
 #endif
-  if (likely(in_pmem(addr))) return pmem_read(addr, len);
-  if(addr >= PSRAM_base && addr < PSRAM_base + PSRAM_size){return psram_read(addr,len);}
-  if(addr >= MROM_base && addr < MROM_base + MROM_size){return mrom_read(addr,len);}
-  if(addr >= SRAM_base && addr < SRAM_base + SRAM_size){return sram_read(addr,len);}
-  if(addr >= FLASH_base && addr < FLASH_base + FLASH_size){return flash_read(addr,len);}
-  if (likely(in_pmem(addr))) return pmem_read(addr, len);
+    return ret;
+  }
   IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
   out_of_bound(addr);
   return 0;
 }
 
 void paddr_write(paddr_t addr, int len, word_t data) {
-#ifdef CONFIG_IMTRACE
-   printf("write at " FMT_PADDR " len=%d, data=" FMT_WORD "\n", addr, len, data);
+  if (likely(in_pmem(addr))) {
+    pmem_write(addr, len, data);
+#ifdef CONFIG_MTRACE
+ if(addr >= CONFIG_MTRACE_START && addr <= CONFIG_MTRACE_START + CONFIG_MTRACE_SIZE) {
+      Log("MTRACE: Write Memory Address: 0x%x, len: %d, data: 0x%x", addr, len, data);
+    }
 #endif
-  if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
-  if(addr >= PSRAM_base && addr < PSRAM_base + PSRAM_size){return psram_write(addr,len,data);}
-  if(addr >= SRAM_base && addr < SRAM_base + SRAM_size){sram_write(addr,len,data);return;}
-  if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
+  return;
+  }
   IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
-  
   out_of_bound(addr);
 }
+
+#else
+
+
+static uint8_t mrom[4*1024] PG_ALIGN = {};
+static uint8_t sram[8*1024] PG_ALIGN = {};
+static uint8_t flash[16*1024*1024] PG_ALIGN = {};
+static uint8_t psram[4*1024*1024] PG_ALIGN = {};
+static uint8_t sdram[64*1024*1024] PG_ALIGN = {};
+
+#ifdef CONFIG_YSYXSOC
+static void out_of_bound(paddr_t addr) {
+  panic("address = " FMT_PADDR " is out of bound of pmem [" FMT_PADDR ", " FMT_PADDR "] at pc = " FMT_WORD,
+      addr, PMEM_LEFT, PMEM_RIGHT, cpu.pc);
+}
+
+uint8_t* guest_to_host(paddr_t paddr) {
+  if(paddr >= 0x0f000000 && paddr <= 0x0fffffff) {
+    return sram + paddr - 0x0f000000;
+  } else if(paddr >= 0x20000000 && paddr <= 0x20000fff) {
+    return mrom + paddr - 0x20000000;
+  } else if(paddr >= 0x30000000 && paddr <= 0x3fffffff) {
+    return flash + paddr - 0x30000000;
+  } else if(paddr >= 0x80000000 && paddr <= 0x9fffffff) {
+    return psram + paddr - 0x80000000;
+  } else if(paddr >= 0xa0000000 && paddr <= 0xbfffffff) {
+    return sdram + paddr - 0xa0000000;
+  } else {
+    out_of_bound(paddr);
+    assert(0);
+  }
+}
+#else
+#if   defined(CONFIG_PMEM_MALLOC)
+static uint8_t *pmem = NULL;
+#else // CONFIG_PMEM_GARRAY
+static uint8_t pmem[CONFIG_MSIZE] PG_ALIGN = {};
+#endif
+uint8_t* guest_to_host(paddr_t paddr) { return pmem + paddr - CONFIG_MBASE; }
+#endif
+
+paddr_t host_to_guest(uint8_t *haddr) { return 0; }
+
+static word_t pmem_read(paddr_t addr, int len) {
+  word_t ret = host_read(guest_to_host(addr), len);
+  // Log(" MTRACE: Read  Memory Address: 0x%x, len: %d, data: 0x%x", addr, len, ret);
+  return ret;
+}
+
+static void pmem_write(paddr_t addr, int len, word_t data) {
+  // Log("MTRACE: Write Memory Address: 0x%x, len: %d, data: 0x%x", addr, len, data);
+  host_write(guest_to_host(addr), len, data);
+}
+
+word_t paddr_read(paddr_t addr, int len) {
+  return pmem_read(addr, len);
+}
+
+void paddr_write(paddr_t addr, int len, word_t data) {
+  pmem_write(addr, len, data);
+}
+
+void init_mem() {
+  IFDEF(CONFIG_MEM_RANDOM, memset(sram, 0, 8*1024));
+  IFDEF(CONFIG_MEM_RANDOM, memset(mrom, rand(), 4*1024));
+  IFDEF(CONFIG_MEM_RANDOM, memset(flash, rand(), 16*1024*1024));
+  IFDEF(CONFIG_MEM_RANDOM, memset(psram, 0, 4*1024*1024));
+  IFDEF(CONFIG_MEM_RANDOM, memset(sdram, 0, 64*1024*1024));
+  Log("physical memory area [" FMT_PADDR ", " FMT_PADDR "]", PMEM_LEFT, PMEM_RIGHT);
+}
+
+#endif
