5295b63253b03fcadd0c892c42f46f0eb0ad1508
tracer-ysyx <tracer@ysyx.org>
2025-08-16 16:17:40 +0800

    > run npc 2023010201031 江贤 Linux jx-virtual-machine 6.5.0-18-generic #18~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb  7 11:40:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:17:40 up  7:27,  1 user,  load average: 0.86, 0.50, 0.49

--- npc/vsrc/lsu.v
+++ npc/vsrc/lsu.v
@@ -254,7 +254,6 @@ always @(posedge clk) begin
                     LSU_AXI4_AWVALID <= 1'b1;
                     LSU_AXI4_WVALID <= 1'b1;
                     LSU_AXI4_wsize <= awsize;
-                    LSU_AXI_wlast <= 1'b1;
                     state <= START;
                 end else if ((EXU_LSU_ren )) begin
                     LSU_AXI4_ARADDR <= EXU_LSU_result;
@@ -267,7 +266,7 @@ always @(posedge clk) begin
                             lsu_during_count <= lsu_during_count + 1'b1;
                             lsu_store_count <= lsu_store_count + 1'b1;
                             lsu_count <= lsu_count + 1'b1;
-                            LSU_AXI_wlast <= 1'b1;
+                            LSU_AXI_wlast <= 1'b0;
                             state <= WRITE_DATA;
                             LSU_AXI4_AWVALID <= 1'b0;
                             LSU_AXI4_WVALID <= 1'b0;
@@ -276,7 +275,7 @@ always @(posedge clk) begin
                             state <= WRITE_WIRE_1;
                             LSU_AXI4_AWVALID <= 1'b0;
                 end else if(LSU_AXI4_WVALID && LSU_AXI4_WREADY ) begin
-                            LSU_AXI_wlast <= 1'b1;
+                            LSU_AXI_wlast <= 1'b0;
                             state <= WRITE_WIRE_2;
                             LSU_AXI4_WVALID <= 1'b0;
                 end else if(LSU_AXI4_ARVALID && LSU_AXI4_ARREADY) begin
