2d8cbb572dcfc6f90066d033f1bfad71e47701aa
tracer-ysyx <tracer@ysyx.org>
2025-08-21 15:36:50 +0800

    > run npc 2023010201031 江贤 Linux jx-virtual-machine 6.5.0-18-generic #18~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb  7 11:40:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:36:50 up  6:40,  1 user,  load average: 1.01, 1.38, 1.34

--- npc/vsrc/icache.v
+++ npc/vsrc/icache.v
@@ -173,8 +173,7 @@ always @(posedge clock) begin
             end
             
             AXI_WAIT: begin
-                if(flush_r) begin state <= IDLE; end
-                else begin
+                if(flush_r) state <= IDLE;
                 ICACHE_AXI4_arvalid <= 1'b1;
                             // 地址对齐到16字节边界
                 if(ICACHE_AXI4_arready && ICACHE_AXI4_arvalid) begin
@@ -186,7 +185,7 @@ always @(posedge clock) begin
                 else begin
                     state <= AXI_WAIT;
                 end
-                end
+
 
             end
             
