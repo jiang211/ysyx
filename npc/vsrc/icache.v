63602c8d13def0b4a70564036c10c5e2b3ddc29a
tracer-ysyx <tracer@ysyx.org>
2025-08-22 10:37:36 +0800

    > run npc 2023010201031 江贤 Linux jx-virtual-machine 6.5.0-18-generic #18~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb  7 11:40:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:37:36 up  1:36,  1 user,  load average: 1.66, 1.31, 1.02

--- npc/vsrc/icache.v
+++ npc/vsrc/icache.v
@@ -274,7 +274,7 @@ end
 
 assign ICACHE_AXI4_rready = 1'b1;
 assign ICACHE_AXI4_arlen = (is_sdram_reg2) ? 2'b11 : 2'b00;  // 一次读取4个数据或1个数据
-assign ICACHE_AXI4_araddr = (is_sdram_reg2) ? {pc_reg2[31:4], 4'b0} : {pc_reg2[31:2], 2'b0};  // 地址对齐到16字节边界
+//assign ICACHE_AXI4_araddr = (is_sdram_reg2) ? {pc_reg2[31:4], 4'b0} : {pc_reg2[31:2], 2'b0};  // 地址对齐到16字节边界
 
 always @(posedge clock)begin
     if(reset) begin
@@ -340,12 +340,14 @@ end
 always @(posedge clock) begin
     if(reset)begin
         ICACHE_AXI4_arvalid <= 0;
+        ICACHE_AXI4_araddr <= 0;
     end
     else if(ICACHE_AXI4_arready && ICACHE_AXI4_arvalid)begin
         ICACHE_AXI4_arvalid <= 1'b0;
     end
     else  if(state == IDLE && (!hit_reg2))begin
         ICACHE_AXI4_arvalid <= 1'b1;
+        ICACHE_AXI4_araddr <= (is_sdram_reg2) ? {pc_reg2[31:4], 4'b0} : {pc_reg2[31:2], 2'b0}; 
     end
     
 end
