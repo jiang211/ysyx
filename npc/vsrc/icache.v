b7a036f6c8064795532f991d4d4f6dd85f0c0741
tracer-ysyx <tracer@ysyx.org>
2025-08-22 15:10:22 +0800

    > run npc 2023010201031 江贤 Linux jx-virtual-machine 6.5.0-18-generic #18~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb  7 11:40:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:10:22 up  6:09,  1 user,  load average: 1.88, 1.14, 1.01

--- npc/vsrc/icache.v
+++ npc/vsrc/icache.v
@@ -163,10 +163,7 @@ always @(posedge clock) begin
     if(reset)begin
         data_valid <= 0;
     end
-    else if(state == IDLE && reg2_valid && hit_reg2) begin
-        data_valid <= 1'b1;
-    end
-    else if(state == UPDATED_CACHE) begin
+    else if((state == IDLE && reg2_valid && hit_reg2) || state == UPDATED_CACHE) begin
         data_valid <= 1'b1;
     end
     else begin
