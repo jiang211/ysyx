78f6f3ed1a9f3e591d8fdce94c73b9fb61de94ae
tracer-ysyx <tracer@ysyx.org>
2025-08-22 15:49:22 +0800

    > run npc 2023010201031 江贤 Linux jx-virtual-machine 6.5.0-18-generic #18~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb  7 11:40:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:49:22 up  6:48,  1 user,  load average: 0.96, 0.74, 0.85

--- npc/vsrc/wbu.v
+++ npc/vsrc/wbu.v
@@ -58,62 +58,17 @@ always @(posedge clk) begin
 end
 
 assign csr_en = LSU_WBU_C_type&(~LSU_WBU_mret)&(~LSU_WBU_ecall);
-always @(posedge clk) begin
-    if(rst_n)begin
-        WBU_REG_DATA <= 32'b0;
-        WBU_REG_ADDR <= 5'b0;
-        WBU_CSR_DATA <= 32'b0;
-        WBU_CSR_ADDR <= 3'b0;
-        WBU_CSR_WEN <= 1'b0;
-        WBU_wen <= 1'b0;
-        WBU_ECALL <= 1'b0;
-        //WBU_IFU_JUMP <= 1'b0;
-        //WBU_IFU_pc <= 32'b0;
-        PC_DATA <= 32'b0;
-        DNPC_DATA <= 32'b0;
-        WBU_TOP_skip <= 1'b0;
-    end else if(LSU_WBU_C_type && LSU_WBU_ready && LSU_WBU_valid) begin
-        WBU_REG_DATA <= LSU_WBU_csr_data;
-        WBU_REG_ADDR   <= addr;
-        WBU_CSR_ADDR   <= LSU_WBU_csr_rst;
-        WBU_CSR_DATA   <= LSU_WBU_csr_in;
-        WBU_wen <= LSU_WBU_reg;
-        WBU_CSR_WEN    <= csr_en;
-        WBU_ECALL      <= LSU_WBU_ecall;
-        PC_DATA        <= LSU_WBU_PC;
-        DNPC_DATA      <= LSU_WBU_dnpc;
-        WBU_TOP_skip   <= LSU_WBU_skip;
-        //WBU_IFU_JUMP   <= LSU_WBU_JUMP;
-        //WBU_IFU_pc     <= LSU_WBU_pc;
-    end else if(LSU_WBU_ready && LSU_WBU_valid) begin
-        WBU_REG_DATA <= LSU_WBU_result;
-        WBU_REG_ADDR   <= addr;
-        WBU_CSR_ADDR   <= LSU_WBU_csr_rst;
-        WBU_CSR_DATA   <= LSU_WBU_csr_in;
-        WBU_wen <= LSU_WBU_reg;
-        WBU_CSR_WEN    <= csr_en;
-        WBU_ECALL      <= LSU_WBU_ecall;
-        PC_DATA        <= LSU_WBU_PC;
-        DNPC_DATA      <= LSU_WBU_dnpc;
-        WBU_TOP_skip   <= LSU_WBU_skip;
-        //WBU_IFU_JUMP   <= LSU_WBU_JUMP;
-        //WBU_IFU_pc     <= LSU_WBU_pc;
-    end else begin
-        
-        WBU_REG_DATA <= 32'b0;
-        WBU_REG_ADDR <= 5'b0;
-        WBU_CSR_DATA <= 32'b0;
-        WBU_CSR_ADDR <= 3'b0;
-        WBU_CSR_WEN <= 1'b0;
-        WBU_wen <= 1'b0;
-        WBU_ECALL <= 1'b0;
-        //WBU_IFU_JUMP <= 1'b0;
-        //WBU_IFU_pc <= 32'b0;
-        PC_DATA <= 32'b0;
-        DNPC_DATA <= 32'b0;
-        WBU_TOP_skip <= 1'b0;
-    end
-end
+assign WBU_wen = LSU_WBU_reg;
+assign WBU_REG_DATA = (LSU_WBU_C_type) ? LSU_WBU_csr_data : LSU_WBU_result;
+assign WBU_REG_ADDR = addr;
+assign WBU_CSR_DATA = LSU_WBU_csr_in;
+assign WBU_CSR_ADDR = LSU_WBU_csr_rst;
+assign WBU_CSR_WEN    = csr_en;
+assign WBU_ECALL      = LSU_WBU_ecall;
+assign PC_DATA        = LSU_WBU_PC;
+assign DNPC_DATA      = LSU_WBU_dnpc;
+assign WBU_TOP_skip   = LSU_WBU_skip;
+
 assign LSU_WBU_ready = 1'b1;
 
 
