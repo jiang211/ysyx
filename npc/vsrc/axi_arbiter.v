7cf55c9ae80dd1f3be4671da4c6c7e187e4c920a
tracer-ysyx <tracer@ysyx.org>
2025-08-23 20:42:25 +0800

    > run npc 2023010201031 江贤 Linux jx-virtual-machine 6.5.0-18-generic #18~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb  7 11:40:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:42:25 up 10:37,  1 user,  load average: 1.33, 1.27, 1.21

--- npc/vsrc/axi_arbiter.v
+++ npc/vsrc/axi_arbiter.v
@@ -9,7 +9,6 @@ module axi_arbiter #(
     input  [ADDR_WIDTH-1:0] ifu_araddr,
     input                   ifu_arvalid,
     output                  ifu_arready,
-    input                   flush,
     
     output [DATA_WIDTH-1:0] ifu_rdata,
     output                  ifu_rvalid,
@@ -120,15 +119,16 @@ always @(posedge clk ) begin
             
             // IFU读操作
             IFU_READ_START: begin
-                if(flush) begin state <= IDLE; end
-                else if (master_arready) begin
+                if (master_arready) begin
                     state <= IFU_READ_WAIT;
+                end
+                else if(!ifu_arvalid) begin
+                    state <= IDLE;
                 end
             end
             
             IFU_READ_WAIT: begin
-                if(flush) begin state <= IDLE; end
-                else if (master_rvalid && master_rready && master_rlast) begin
+                if (master_rvalid && master_rready && master_rlast) begin
                     state <= IDLE;
                 end
             end
