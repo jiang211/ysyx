4e611a9d00ea16b0df1f7dadecf50f88d8faa4b8
tracer-ysyx <tracer@ysyx.org>
2025-08-25 13:52:12 +0800

    > run npc 2023010201031 江贤 Linux jx-virtual-machine 6.5.0-18-generic #18~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb  7 11:40:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:52:12 up 27 min,  1 user,  load average: 0.99, 0.55, 0.39

--- npc/csrc/isa.c
+++ npc/csrc/isa.c
@@ -5,8 +5,10 @@
 
 static const uint32_t img [] = {
   0x00000297,  // auipc t0,0
-  0x00028823,  // sb  zero,16(t0)
-  0x0102c503,  // lbu a0,16(t0)
+  0x70009117,  // sb  zero,16(t0)
+  0xffc10113,  // lbu a0,16(t0)
+  0xffc10113,  // lbu a0,16(t0)
+  0xffc10113,  // lbu a0,16(t0)
   0x00100073,  // ebreak (used as nemu_trap)
   0xdeadbeef,  // some data
 };
