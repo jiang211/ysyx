42bb625e22661c2a0fef8800b186d08b54ea9250
tracer-ysyx <tracer@ysyx.org>
2025-08-25 16:14:29 +0800

    > run npc 2023010201031 江贤 Linux jx-virtual-machine 6.5.0-18-generic #18~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb  7 11:40:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:14:29 up  2:50,  1 user,  load average: 0.87, 0.51, 0.69

--- npc/vsrc/raw.v
+++ npc/vsrc/raw.v
@@ -34,13 +34,16 @@ module raw (
     wire rs1_raw_lsu1 = conflict(idu_rs1_addr, idu_rs1_valid, lsu_rd_addr, lsu_reg_write_en);
     wire rs2_raw_lsu1 = conflict(idu_rs2_addr, idu_rs2_valid, lsu_rd_addr, lsu_reg_write_en);
 
-    assign rs1_raw_exu = conflict(idu_rs1_addr, idu_rs1_valid, exu_rd_addr, exu_reg_write_en);
-    assign rs2_raw_exu = conflict(idu_rs2_addr, idu_rs2_valid, exu_rd_addr, exu_reg_write_en);
+    wire rs1_raw_exu1 = conflict(idu_rs1_addr, idu_rs1_valid, exu_rd_addr, exu_reg_write_en);
+    wire rs2_raw_exu1 = conflict(idu_rs2_addr, idu_rs2_valid, exu_rd_addr, exu_reg_write_en);
+
+    assign rs1_raw_exu = rs1_raw_exu1 && (!lsu_ren);
+    assign rs2_raw_exu = rs2_raw_exu1 && (!lsu_ren);
 
     assign rs1_raw_lsu = rs1_raw_lsu1 && (!lsu_ren);
     assign rs2_raw_lsu = rs2_raw_lsu1 && (!lsu_ren);
 
-    assign stall         = (rs1_raw_lsu1 | rs2_raw_lsu1) && lsu_ren ;
+    assign stall         = (rs1_raw_exu1 | rs2_raw_exu1) && lsu_ren ;
                             //rs1_raw_wbu | rs2_raw_wbu ;
     
 endmodule
