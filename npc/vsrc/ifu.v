3e23a937162b4612743509e0cb7dfea5aa464373
tracer-ysyx <tracer@ysyx.org>
2025-08-25 19:27:07 +0800

    > run npc 2023010201031 江贤 Linux jx-virtual-machine 6.5.0-18-generic #18~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb  7 11:40:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:27:07 up  6:02,  1 user,  load average: 1.69, 1.10, 0.90

--- npc/vsrc/ifu.v
+++ npc/vsrc/ifu.v
@@ -52,7 +52,7 @@ begin
     else if(EXU_IFU_flush)begin 
     pc <= EXU_IFU_pc;
     end
-    else if(stall) begin
+    else if(stall || fence_i) begin
     pc <= pc;
     end
     else if(IDU_IFU_ready && (~ICACHE_IFU_stall))begin
