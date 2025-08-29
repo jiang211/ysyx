3659bb70fe200791fe72657972eb4668fbd6d0f5
tracer-ysyx <tracer@ysyx.org>
2025-08-29 09:18:33 +0800

    > run npc 2023010201031 江贤 Linux jx-virtual-machine 6.5.0-18-generic #18~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb  7 11:40:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:18:33 up 4 min,  1 user,  load average: 0.67, 0.99, 0.52

--- npc/vsrc/ysyx_24070003_cpu.v
+++ npc/vsrc/ysyx_24070003_cpu.v
@@ -2695,136 +2695,136 @@ end
 end
 endmodule
 
-//`include "defines.v"
+
 module ysyx_24070003_exuop_ctrl(
     input [2:0]  funct3,
     input [5:0]  funct7,
     input [1:0]  alu_op,
     output reg [3:0] aluOp
 );
-`define OP_ADD         4'b0000 // +
-`define OP_SUB         4'b0001 // -
-`define OP_SLL         4'b0010 // <<
-`define OP_SLT         4'b0011 // <
-`define OP_XOR         4'b0100 // ^
-`define OP_SRL         4'b0101 // >>
-`define OP_SRA         4'b0110 // >>>
-`define OP_OR          4'b0111 // |
-`define OP_AND         4'b1000 // &
-`define OP_MU          4'b1001 // *
-`define OP_DIV         4'b1010 // /
-`define OP_REM         4'b1011 // %
-`define OP_BGE         4'b1100 // >=
-`define OP_BNE         4'b1101 // !=
-`define OP_BEQ         4'b1110 // ==
-`define MUL_H          4'b1111 // *
+`define ysyx_24070003_OP_ADD         4'b0000 // +
+`define ysyx_24070003_OP_SUB         4'b0001 // -
+`define ysyx_24070003_OP_SLL         4'b0010 // <<
+`define ysyx_24070003_OP_SLT         4'b0011 // <
+`define ysyx_24070003_OP_XOR         4'b0100 // ^
+`define ysyx_24070003_OP_SRL         4'b0101 // >>
+`define ysyx_24070003_OP_SRA         4'b0110 // >>>
+`define ysyx_24070003_OP_OR          4'b0111 // |
+`define ysyx_24070003_OP_AND         4'b1000 // &
+`define ysyx_24070003_OP_MU          4'b1001 // *
+`define ysyx_24070003_OP_DIV         4'b1010 // /
+`define ysyx_24070003_OP_REM         4'b1011 // %
+`define ysyx_24070003_OP_BGE         4'b1100 // >=
+`define ysyx_24070003_OP_BNE         4'b1101 // !=
+`define ysyx_24070003_OP_BEQ         4'b1110 // ==
+`define ysyx_24070003_MUL_H          4'b1111 // *
 
-`define BEQ_FUNCT3         3'b000
-`define BNE_FUNCT3         3'b001
-`define BLT_FUNCT3         3'b100
-`define BGE_FUNCT3         3'b101
-`define BLTU_FUNCT3        3'b110
-`define BGEU_FUNCT3        3'b111
+`define ysyx_24070003_BEQ_FUNCT3         3'b000
+`define ysyx_24070003_BNE_FUNCT3         3'b001
+`define ysyx_24070003_BLT_FUNCT3         3'b100
+`define ysyx_24070003_BGE_FUNCT3         3'b101
+`define ysyx_24070003_BLTU_FUNCT3        3'b110
+`define ysyx_24070003_BGEU_FUNCT3        3'b111
 
-`define ADDI_FUNCT3         3'b000
-`define SLLI_FUNCT3         3'b001
-`define SLTI_FUNCT3         3'b010
-`define SLTIU_FUNCT3        3'b011
-`define XORI_FUNCT3         3'b100
-`define SRLI_SRAI_FUNCT3    3'b101
-`define ORI_FUNCT3          3'b110
-`define ANDI_FUNCT3         3'b111
+`define ysyx_24070003_ADDI_FUNCT3         3'b000
+`define ysyx_24070003_SLLI_FUNCT3         3'b001
+`define ysyx_24070003_SLTI_FUNCT3         3'b010
+`define ysyx_24070003_SLTIU_FUNCT3        3'b011
+`define ysyx_24070003_XORI_FUNCT3         3'b100
+`define ysyx_24070003_SRLI_SRAI_FUNCT3    3'b101
+`define ysyx_24070003_ORI_FUNCT3          3'b110
+`define ysyx_24070003_ANDI_FUNCT3         3'b111
 
-`define SUB_ADD_MUL_FUNCT3         3'b000
-`define SLL_MULH_FUNCT3            3'b001
-`define SLT_FUNCT3                 3'b010
-`define SLTU_MULHU_FUNCT3          3'b011
-`define XOR_DIV_FUNCT3             3'b100
-`define SRL_SRA_DIVU_FUNCT3        3'b101
-`define OR_REM_FUNCT3              3'b110
-`define AND_REMU_FUNCT3            3'b111
+`define ysyx_24070003_SUB_ADD_MUL_FUNCT3         3'b000
+`define ysyx_24070003_SLL_MULH_FUNCT3            3'b001
+`define ysyx_24070003_SLT_FUNCT3                 3'b010
+`define ysyx_24070003_SLTU_MULHU_FUNCT3          3'b011
+`define ysyx_24070003_XOR_DIV_FUNCT3             3'b100
+`define ysyx_24070003_SRL_SRA_DIVU_FUNCT3        3'b101
+`define ysyx_24070003_OR_REM_FUNCT3              3'b110
+`define ysyx_24070003_AND_REMU_FUNCT3            3'b111
     always @(*) begin
       case(alu_op)
-        2'b00:  aluOp = `OP_ADD;       //load/store    
+        2'b00:  aluOp = `ysyx_24070003_OP_ADD;       //load/store    
         2'b10:  begin //R
           case(funct3)                   
-            `SUB_ADD_MUL_FUNCT3: begin
+            `ysyx_24070003_SUB_ADD_MUL_FUNCT3: begin
                   if(funct7[5]) //sub
-                    aluOp = `OP_SUB;
+                    aluOp = `ysyx_24070003_OP_SUB;
                   else if(funct7[0]) //mul
-                    aluOp = `OP_MU;
+                    aluOp = `ysyx_24070003_OP_MU;
                   else //add
-                    aluOp = `OP_ADD;
+                    aluOp = `ysyx_24070003_OP_ADD;
               end
-            `SLL_MULH_FUNCT3: begin
+            `ysyx_24070003_SLL_MULH_FUNCT3: begin
                   if(funct7[0]) //mulh
-                    aluOp = `OP_MU;
+                    aluOp = `ysyx_24070003_OP_MU;
                   else //sll
-                    aluOp = `OP_SLL;
+                    aluOp = `ysyx_24070003_OP_SLL;
               end
-            `SLT_FUNCT3: begin
-                  aluOp = `OP_SLT;
+            `ysyx_24070003_SLT_FUNCT3: begin
+                  aluOp = `ysyx_24070003_OP_SLT;
               end
-            `SLTU_MULHU_FUNCT3: begin
+            `ysyx_24070003_SLTU_MULHU_FUNCT3: begin
                   if(funct7[0]) //mulhu
-                    aluOp = `OP_MU;
+                    aluOp = `ysyx_24070003_OP_MU;
                   else //sltu
-                    aluOp = `OP_SLT;
+                    aluOp = `ysyx_24070003_OP_SLT;
               end
-            `XOR_DIV_FUNCT3: begin
+            `ysyx_24070003_XOR_DIV_FUNCT3: begin
                   if(funct7[0]) //div
-                    aluOp = `OP_DIV;
+                    aluOp = `ysyx_24070003_OP_DIV;
                   else //xor
-                    aluOp = `OP_XOR;
+                    aluOp = `ysyx_24070003_OP_XOR;
               end
-            `SRL_SRA_DIVU_FUNCT3: begin
+            `ysyx_24070003_SRL_SRA_DIVU_FUNCT3: begin
                   if(funct7[5]) //sra
-                    aluOp = `OP_SRA;
+                    aluOp = `ysyx_24070003_OP_SRA;
                   else if(funct7[0]) //divu
-                    aluOp = `OP_DIV;
+                    aluOp = `ysyx_24070003_OP_DIV;
                   else //srl
-                    aluOp = `OP_SRL;
+                    aluOp = `ysyx_24070003_OP_SRL;
               end
-            `OR_REM_FUNCT3: begin
+            `ysyx_24070003_OR_REM_FUNCT3: begin
                   if(funct7[0]) //rem
-                    aluOp = `OP_REM;
+                    aluOp = `ysyx_24070003_OP_REM;
                   else //or
-                    aluOp = `OP_OR;
+                    aluOp = `ysyx_24070003_OP_OR;
               end
-            `AND_REMU_FUNCT3: begin
+            `ysyx_24070003_AND_REMU_FUNCT3: begin
                   if(funct7[0]) //remu
-                    aluOp = `OP_REM;
+                    aluOp = `ysyx_24070003_OP_REM;
                   else //and
-                    aluOp = `OP_AND;
+                    aluOp = `ysyx_24070003_OP_AND;
               end
-            default : aluOp = `OP_ADD;
+            default : aluOp = `ysyx_24070003_OP_ADD;
         endcase
         end
         2'b01:  begin //B
           case(funct3)                   
-            `BEQ_FUNCT3:  aluOp = `OP_BEQ;
-            `BNE_FUNCT3:  aluOp = `OP_BNE;
-            `BLT_FUNCT3:  aluOp = `OP_SLT;
-            `BGE_FUNCT3:  aluOp = `OP_BGE;
-            `BLTU_FUNCT3: aluOp = `OP_SLT;
-            `BGEU_FUNCT3: aluOp = `OP_BGE;
-            default:      aluOp = `OP_ADD;
+            `ysyx_24070003_BEQ_FUNCT3:  aluOp = `ysyx_24070003_OP_BEQ;
+            `ysyx_24070003_BNE_FUNCT3:  aluOp = `ysyx_24070003_OP_BNE;
+            `ysyx_24070003_BLT_FUNCT3:  aluOp = `ysyx_24070003_OP_SLT;
+            `ysyx_24070003_BGE_FUNCT3:  aluOp = `ysyx_24070003_OP_BGE;
+            `ysyx_24070003_BLTU_FUNCT3: aluOp = `ysyx_24070003_OP_SLT;
+            `ysyx_24070003_BGEU_FUNCT3: aluOp = `ysyx_24070003_OP_BGE;
+            default:      aluOp = `ysyx_24070003_OP_ADD;
           endcase
           end
         2'b11:  begin //I
         case(funct3)                   
-            `ADDI_FUNCT3: aluOp = `OP_ADD;
-            `SLLI_FUNCT3: aluOp = `OP_SLL;
-            `SLTI_FUNCT3: aluOp = `OP_SLT;
-            `SLTIU_FUNCT3: aluOp = `OP_SLT;
-            `XORI_FUNCT3: aluOp = `OP_XOR;
-            `SRLI_SRAI_FUNCT3: begin if(funct7[5]) aluOp = `OP_SRA;else aluOp = `OP_SRL; end
-            `ORI_FUNCT3: aluOp = `OP_OR;
-            `ANDI_FUNCT3: aluOp = `OP_AND;
+            `ysyx_24070003_ADDI_FUNCT3: aluOp = `ysyx_24070003_OP_ADD;
+            `ysyx_24070003_SLLI_FUNCT3: aluOp = `ysyx_24070003_OP_SLL;
+            `ysyx_24070003_SLTI_FUNCT3: aluOp = `ysyx_24070003_OP_SLT;
+            `ysyx_24070003_SLTIU_FUNCT3: aluOp = `ysyx_24070003_OP_SLT;
+            `ysyx_24070003_XORI_FUNCT3: aluOp = `ysyx_24070003_OP_XOR;
+            `ysyx_24070003_SRLI_SRAI_FUNCT3: begin if(funct7[5]) aluOp = `ysyx_24070003_OP_SRA;else aluOp = `ysyx_24070003_OP_SRL; end
+            `ysyx_24070003_ORI_FUNCT3: aluOp = `ysyx_24070003_OP_OR;
+            `ysyx_24070003_ANDI_FUNCT3: aluOp = `ysyx_24070003_OP_AND;
             default: aluOp = funct7[4:1];
         endcase
         end
-        default: aluOp = `OP_ADD;
+        default: aluOp = `ysyx_24070003_OP_ADD;
       endcase
     end
     /*
