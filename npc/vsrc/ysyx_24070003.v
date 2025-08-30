76970e9d3018314262e9b3880beb2fb2f8aea562
tracer-ysyx <tracer@ysyx.org>
2025-08-30 16:11:58 +0800

    > run npc 2023010201031 江贤 Linux jx-virtual-machine 6.5.0-18-generic #18~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb  7 11:40:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:11:58 up  6:47,  1 user,  load average: 1.04, 0.61, 0.67

--- npc/vsrc/ysyx_24070003.v
+++ npc/vsrc/ysyx_24070003.v
@@ -904,33 +904,33 @@ always @(posedge clock ) begin
         total_count <= total_count + 1;
     end
 end
-// always @(posedge clock ) begin
-//     if(EXU_LSU_ebreak) begin
-//         $display("total_count               = %040d\n",total_count);
-//         $display("total_instr               = %040d\n",calcu_type_count + Jump_type_count + LOAD_type_count + STORE_type_count + C_type_count);
-//         $display("lsu_count                 = %040d\n",lsu_count);
-//         $display("ifu_count                 = %040d\n",ifu_count);
-//         $display("calcu_type_count          = %040d\n",calcu_type_count);
-//         $display("Jump_type_count           = %040d\n",Jump_type_count);
-//         $display("BJump_type_count          = %040d\n",BJump_type_count);
-//         $display("LOAD_type_count           = %040d\n",LOAD_type_count);
-//         $display("STORE_type_count          = %040d\n",STORE_type_count);
-//         $display("C_type_count              = %040d\n",C_type_count);
-//         $display("lsu_average_count         = %040d\n",lsu_during_count/lsu_count);
-//         $display("ifu_average_count         = %040d\n",ifu_during_count/ifu_count);
-//         $display("load_instr_average_count  = %040d\n",lsu_load_count/LOAD_type_count);
-//         $display("store_instr_average_count = %040d\n",lsu_store_count/STORE_type_count);
-//         $display("ICACHE hit_count          = %040d\n",ICACHE_hit_count);
-//         $display("ICACHE miss_count         = %040d\n",ICACHE_miss_count);
-//         $display("total_access              = %040d\n",total_access);
-//         $display("access_time               = %040d\n",access_time);
-//         $display("miss_penalty              = %040d\n",miss_penalty);
-//     end
-// end
-// import "DPI-C" function void set_monitor_ptr(input logic [31:0] data []);
-// reg [31:0] dpi_monitor_data[0:5];
-// // 初始化时绑定指针
-// initial set_monitor_ptr(dpi_monitor_data);
+always @(posedge clock ) begin
+    if(EXU_LSU_ebreak) begin
+        $display("total_count               = %040d\n",total_count);
+        $display("total_instr               = %040d\n",calcu_type_count + Jump_type_count + LOAD_type_count + STORE_type_count + C_type_count);
+        $display("lsu_count                 = %040d\n",lsu_count);
+        $display("ifu_count                 = %040d\n",ifu_count);
+        $display("calcu_type_count          = %040d\n",calcu_type_count);
+        $display("Jump_type_count           = %040d\n",Jump_type_count);
+        $display("BJump_type_count          = %040d\n",BJump_type_count);
+        $display("LOAD_type_count           = %040d\n",LOAD_type_count);
+        $display("STORE_type_count          = %040d\n",STORE_type_count);
+        $display("C_type_count              = %040d\n",C_type_count);
+        $display("lsu_average_count         = %040d\n",lsu_during_count/lsu_count);
+        $display("ifu_average_count         = %040d\n",ifu_during_count/ifu_count);
+        $display("load_instr_average_count  = %040d\n",lsu_load_count/LOAD_type_count);
+        $display("store_instr_average_count = %040d\n",lsu_store_count/STORE_type_count);
+        $display("ICACHE hit_count          = %040d\n",ICACHE_hit_count);
+        $display("ICACHE miss_count         = %040d\n",ICACHE_miss_count);
+        $display("total_access              = %040d\n",total_access);
+        $display("access_time               = %040d\n",access_time);
+        $display("miss_penalty              = %040d\n",miss_penalty);
+    end
+end
+import "DPI-C" function void set_monitor_ptr(input logic [31:0] data []);
+reg [31:0] dpi_monitor_data[0:5];
+// 初始化时绑定指针
+initial set_monitor_ptr(dpi_monitor_data);
 assign dpi_monitor_data[0] = {31'b0,difftest_valid};
 assign dpi_monitor_data[1] = TO_top_pc;
 assign dpi_monitor_data[2] = TO_top_dnpc;
