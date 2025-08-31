535e42cabacce59fe548bfba1b9409dfb4a28a5a
tracer-ysyx <tracer@ysyx.org>
2025-08-31 19:31:17 +0800

    > run npc 2023010201031 江贤 Linux jx-virtual-machine 6.5.0-18-generic #18~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb  7 11:40:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:31:17 up 10:16,  1 user,  load average: 1.48, 0.99, 1.09

--- npc/vsrc/ysyx_24070003_alu.v
+++ npc/vsrc/ysyx_24070003_alu.v
@@ -100,7 +100,7 @@ assign BEQ = (alu_crtl[3]  & ~alu_crtl[2]  &   alu_crtl[1]  &  alu_crtl[0]);
 assign zero = (BLT &  LESS_S)   |
               (BGE & ~LESS_S)   |
               (BNE & ~ADD_zero) |
-              (BEQ &  ADD_zero);
+              (BEQ &  ADD_zero) & branch;
 
 assign LESS_M1 = ADD_carry ^ sub_ctl;
 assign LESS_M2 = ADD_OverFlow ^ ADD_result[31];
