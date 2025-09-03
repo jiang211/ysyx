4b7f57d8dec326ac2ca10c572a015db5b07b1616
tracer-ysyx <tracer@ysyx.org>
2025-09-03 15:42:11 +0800

    > run npc 2023010201031 江贤 Linux jx-virtual-machine 6.5.0-18-generic #18~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb  7 11:40:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:42:11 up  2:36,  1 user,  load average: 1.64, 1.44, 1.15

--- ysyxSoC/perip/spi/rtl/spi_top_apb.v
+++ ysyxSoC/perip/spi/rtl/spi_top_apb.v
@@ -334,7 +334,7 @@ assign in_pready  = ((is_flash&(state==XIP_RETURN))
                   |(is_spi&(state==IDLE)))
                   &(wb_pready)
                   ;
-assign in_prdata  = ((state_r==IDLE)|(state_r==XIP_RETURN))?data_return:'b0;
+assign in_prdata  = data_return;
 assign in_pslverr = 'b0;
 spi_top u0_spi_top (
   .wb_clk_i(clock),
