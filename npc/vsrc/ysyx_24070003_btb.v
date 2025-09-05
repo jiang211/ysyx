a8680e776a130eb95650c1175313e374ec454e24
tracer-ysyx <tracer@ysyx.org>
2025-09-05 20:42:01 +0800

    > run npc ysyx_24070003 江贤 Linux jx-virtual-machine 6.5.0-18-generic #18~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb  7 11:40:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:42:01 up  6:56,  1 user,  load average: 1.00, 1.08, 1.21

--- npc/vsrc/ysyx_24070003_btb.v
+++ npc/vsrc/ysyx_24070003_btb.v
@@ -47,7 +47,7 @@ assign update_hit[1] = ~btb_valid[update_index][1] ;
 
 
 
-assign pred_pc = (hit[0]) ? {btb_target[cur_index][0],2'b00} : (hit[1]) ? {btb_target[cur_index][1],2'b00} : cur_pc;
+assign pred_pc = (pred_valid) ? {btb_target[cur_index][hit[1]],2'b00} : cur_pc;
 
 reg update_way;
 always @(posedge clock) begin
