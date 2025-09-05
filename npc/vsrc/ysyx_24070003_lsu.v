bd75190aeb4098e38d735e7ac8e03843ae7a0556
tracer-ysyx <tracer@ysyx.org>
2025-09-05 16:00:02 +0800

    > run npc ysyx_24070003 江贤 Linux jx-virtual-machine 6.5.0-18-generic #18~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb  7 11:40:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:00:02 up  2:14,  1 user,  load average: 0.82, 1.00, 1.30

--- npc/vsrc/ysyx_24070003_lsu.v
+++ npc/vsrc/ysyx_24070003_lsu.v
@@ -133,11 +133,11 @@ reg [2:0] state;
                    ( {32{EXU_LSU_sh}} & {16'b0,rs2_data[15:0]}) |
                    ( {32{EXU_LSU_sw}} & rs2_data);
 
-    assign lsu_rdata = ((LSU_AXI4_ARADDR[1:0] & 2'b11) == 2'b00) ? (LSU_RDATA >> 32'd0) :
-                       ((LSU_AXI4_ARADDR[1:0] & 2'b11) == 2'b01) ? (LSU_RDATA >> 32'd8) :
-                       ((LSU_AXI4_ARADDR[1:0] & 2'b11) == 2'b10) ? (LSU_RDATA >> 32'd16) :
-                       ((LSU_AXI4_ARADDR[1:0] & 2'b11) == 2'b11) ? (LSU_RDATA >> 32'd24) : (LSU_RDATA >> 32'd0);
-    //assign lsu_rdata = (LSU_RDATA >> (LSU_AXI4_ARADDR[1:0] << 3));
+    // assign lsu_rdata = ((LSU_AXI4_ARADDR[1:0] & 2'b11) == 2'b00) ? (LSU_RDATA >> 32'd0) :
+    //                    ((LSU_AXI4_ARADDR[1:0] & 2'b11) == 2'b01) ? (LSU_RDATA >> 32'd8) :
+    //                    ((LSU_AXI4_ARADDR[1:0] & 2'b11) == 2'b10) ? (LSU_RDATA >> 32'd16) :
+    //                    ((LSU_AXI4_ARADDR[1:0] & 2'b11) == 2'b11) ? (LSU_RDATA >> 32'd24) : (LSU_RDATA >> 32'd0);
+    assign lsu_rdata = (LSU_RDATA >> (LSU_AXI4_ARADDR[1:0] << 3));
 
     //assign lsu_rdata_ = ((LSU_AXI4_ARADDR >= 32'h30000000) && (LSU_AXI4_ARADDR < 32'h40000000)) ? LSU_RDATA : lsu_rdata;
     assign rdata = ( {32{LSU_WBU_lb}} & {{24{lsu_rdata[7]}},lsu_rdata[7:0]}) |
@@ -173,11 +173,11 @@ reg [2:0] state;
     //                    (EXU_LSU_result[1:0]  == 2'b10) ? (LSU_WLEN << 2'd2) :
     //                    (EXU_LSU_result[1:0]  == 2'b11) ? (LSU_WLEN << 2'd3) : (LSU_WLEN << 2'd0);
      assign lsu_wstrb = (LSU_WLEN << EXU_LSU_result[1:0] );
-    // assign lsu_wdata = (EXU_LSU_result[1:0]  == 2'b00) ? (LSU_WDATA << 32'd0) :
-    //                    (EXU_LSU_result[1:0]  == 2'b01) ? (LSU_WDATA << 32'd8) :
-    //                    (EXU_LSU_result[1:0]  == 2'b10) ? (LSU_WDATA << 32'd16) :
-    //                    (EXU_LSU_result[1:0]  == 2'b11) ? (LSU_WDATA << 32'd24) : (LSU_WDATA << 32'd0);
-    assign lsu_wdata = (LSU_WDATA << (EXU_LSU_result[1:0] << 'd3));
+    assign lsu_wdata = (EXU_LSU_result[1:0]  == 2'b00) ? (LSU_WDATA << 32'd0) :
+                       (EXU_LSU_result[1:0]  == 2'b01) ? (LSU_WDATA << 32'd8) :
+                       (EXU_LSU_result[1:0]  == 2'b10) ? (LSU_WDATA << 32'd16) :
+                       (EXU_LSU_result[1:0]  == 2'b11) ? (LSU_WDATA << 32'd24) : (LSU_WDATA << 32'd0);
+    //assign lsu_wdata = (LSU_WDATA << (EXU_LSU_result[1:0] << 3));
     //wire [31:0] LSU_WBU_DATA;
     reg [31:0] LSU_WBU_result;
     assign LSU_WBU_DATA = (LSU_REN) ? rdata : LSU_WBU_result;
