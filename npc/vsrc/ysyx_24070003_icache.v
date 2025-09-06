9ac97606296d28742d3b04c9ce9702879456b3bd
tracer-ysyx <tracer@ysyx.org>
2025-09-06 23:17:41 +0800

    > run npc ysyx_24070003 江贤 Linux jx-virtual-machine 6.5.0-18-generic #18~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb  7 11:40:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:17:41 up  9:37,  1 user,  load average: 2.32, 1.64, 1.25

--- npc/vsrc/ysyx_24070003_icache.v
+++ npc/vsrc/ysyx_24070003_icache.v
@@ -233,6 +233,7 @@ end
 
 
 assign ICACHE_AXI4_araddr = {pc_reg1[31:4], 4'b0} ; 
+assign ICACHE_AXI4_arvalid = (state == IDLE && (!hit_reg1) && reg1_valid && (!flush) && IFU_AXI4_rready);
 assign ICACHE_AXI4_rready = 1'b1;
 assign ICACHE_AXI4_arlen = 8'b11;  // 一次读取4个数据
 //assign ICACHE_AXI4_araddr = (is_sdram_reg2) ? {pc_reg2[31:4], 4'b0} : {pc_reg2[31:2], 2'b0};  // 地址对齐到16字节边界
@@ -300,19 +301,19 @@ end
 //     end
 // end
 
-always @(posedge clock) begin
-    if(reset)begin
-        ICACHE_AXI4_arvalid <= 0;
-    end
-    else if((ICACHE_AXI4_arready && ICACHE_AXI4_arvalid))begin
-        ICACHE_AXI4_arvalid <= 1'b0;
-    end
-    else  if(state == IDLE && (!hit_reg1) && reg1_valid && (!flush) && IFU_AXI4_rready)begin
-        ICACHE_AXI4_arvalid <= 1'b1;
+// always @(posedge clock) begin
+//     if(reset)begin
+//         ICACHE_AXI4_arvalid <= 0;
+//     end
+//     else if((ICACHE_AXI4_arready && ICACHE_AXI4_arvalid))begin
+//         ICACHE_AXI4_arvalid <= 1'b0;
+//     end
+//     else  if(state == IDLE && (!hit_reg1) && reg1_valid && (!flush) && IFU_AXI4_rready)begin
+//         ICACHE_AXI4_arvalid <= 1'b1;
         
-    end
+//     end
     
-end
+// end
 
 
 
