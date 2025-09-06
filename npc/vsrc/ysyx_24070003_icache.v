67d948e0b7038d93b82968ed7be0a665f5a1710f
tracer-ysyx <tracer@ysyx.org>
2025-09-06 23:36:11 +0800

    > run npc ysyx_24070003 江贤 Linux jx-virtual-machine 6.5.0-18-generic #18~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb  7 11:40:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:36:11 up  9:56,  1 user,  load average: 1.06, 0.84, 1.02

--- npc/vsrc/ysyx_24070003_icache.v
+++ npc/vsrc/ysyx_24070003_icache.v
@@ -144,10 +144,10 @@ always @(posedge clock) begin
     end
     else if((!(LSU_IFU_stall || IDU_IFU_STALL)) && state == UPDATED_CACHE)begin
         case (addr_buffer[3:2])
-            2'b00: data_reg3 <= data[index_reg2][31:0];
-            2'b01: data_reg3 <= data[index_reg2][63:32];
-            2'b10: data_reg3 <= data[index_reg2][95:64];
-            2'b11: data_reg3 <= data[index_reg2][127:96];
+            2'b00: data_reg3 <= burst_buffer[31:0];
+            2'b01: data_reg3 <= burst_buffer[63:32];
+            2'b10: data_reg3 <= burst_buffer[95:64];
+            2'b11: data_reg3 <= burst_buffer[127:96];
         endcase
         addr_reg3 <= addr_buffer;
         per_pc_reg3 <= pre_pc_buffer;
@@ -301,10 +301,10 @@ always @(posedge clock)begin
     end 
     else if(state == AXI_READ && ICACHE_AXI4_rvalid) begin
         case (burst_count)
-            2'b00: data[index_buffer][31:0] <= ICACHE_AXI4_rdata;
-            2'b01: data[index_buffer][63:32] <= ICACHE_AXI4_rdata;
-            2'b10: data[index_buffer][95:64] <= ICACHE_AXI4_rdata;
-            2'b11: data[index_buffer][127:96] <= ICACHE_AXI4_rdata;
+            2'b00: burst_buffer[31:0] <= ICACHE_AXI4_rdata;
+            2'b01: burst_buffer[63:32] <= ICACHE_AXI4_rdata;
+            2'b10: burst_buffer[95:64] <= ICACHE_AXI4_rdata;
+            2'b11: burst_buffer[127:96] <= ICACHE_AXI4_rdata;
         endcase
         
     end
@@ -353,6 +353,7 @@ end
 always @(posedge clock) begin
     if(state == UPDATED_CACHE) begin
         tags[index_buffer] <= tag_buffer;
+        data[index_buffer] <= burst_buffer;
         
     end
 end
