ff82946baa8d8bbb726ca027a47c71c4eb5fae56
tracer-ysyx <tracer@ysyx.org>
2025-09-07 14:03:12 +0800

    > run npc ysyx_24070003 江贤 Linux jx-virtual-machine 6.5.0-18-generic #18~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb  7 11:40:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:03:12 up 56 min,  1 user,  load average: 1.30, 1.09, 1.00

--- npc/vsrc/ysyx_24070003_icache.v
+++ npc/vsrc/ysyx_24070003_icache.v
@@ -125,12 +125,12 @@ always @(posedge clock) begin
             per_pc_reg3 <= reg1_pre_dnpc;
         end
     end
-    else if((!(LSU_IFU_stall || IDU_IFU_STALL)) && ICACHE_AXI4_rlast)begin
+    else if((!(LSU_IFU_stall || IDU_IFU_STALL)) && state == UPDATED_CACHE)begin
         case (addr_buffer[3:2])
             2'b00: data_reg3 <= data[index_buffer][31:0];
             2'b01: data_reg3 <= data[index_buffer][63:32];
             2'b10: data_reg3 <= data[index_buffer][95:64];
-            2'b11: data_reg3 <= ICACHE_AXI4_rdata;
+            2'b11: data_reg3 <= data[index_buffer][127:96];
         endcase
         addr_reg3 <= addr_buffer;
         per_pc_reg3 <= pre_pc_buffer;
@@ -155,7 +155,7 @@ always @(posedge clock) begin
     else if(LSU_IFU_stall || IDU_IFU_STALL) begin
         data_valid <= data_valid;
     end
-    else if((state == IDLE && reg1_valid && hit_reg1 && (~flush)) || (ICACHE_AXI4_rlast)) begin
+    else if((state == IDLE && reg1_valid && hit_reg1 && (~flush)) || (state == UPDATED_CACHE)) begin
         data_valid <= 1'b1;
     end
     else begin
@@ -201,7 +201,7 @@ always @(posedge clock) begin
         end
         AXI_READ: begin
             if(ICACHE_AXI4_rvalid && ICACHE_AXI4_rlast) begin
-                state <= IDLE;
+                state <= UPDATED_CACHE;
             end
             else begin
                 state <= AXI_READ;
@@ -320,13 +320,19 @@ always @(posedge clock) begin
             valid[i] <= 0;  // 复位时所有块无效
         end
     end
-    else if(state == AXI_READ) begin
+    else if(state == UPDATED_CACHE) begin
         valid[index_buffer] <= 1'b1;
         tags[index_buffer] <= tag_buffer;
     end
 end
 
-
+// always @(posedge clock) begin
+//     if(state == UPDATED_CACHE) begin
+//         tags[index_buffer] <= tag_buffer;
+//         data[index_buffer] <= burst_buffer;
+        
+//     end
+// end
 
 always @(posedge clock) begin
     if(reset)begin
