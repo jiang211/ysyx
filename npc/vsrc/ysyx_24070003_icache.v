754537ad13839cef00e8486f8a58e603c57f4f91
tracer-ysyx <tracer@ysyx.org>
2025-09-07 16:19:13 +0800

    > run npc ysyx_24070003 江贤 Linux jx-virtual-machine 6.5.0-18-generic #18~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb  7 11:40:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:19:13 up  3:12,  1 user,  load average: 1.95, 1.89, 1.53

--- npc/vsrc/ysyx_24070003_icache.v
+++ npc/vsrc/ysyx_24070003_icache.v
@@ -23,7 +23,7 @@ module ysyx_24070003_icache(
     // output reg [63:0] miss_penalty,
     // output reg [63:0] ifu_during_count,
 
-    output reg [31:0] ICACHE_AXI4_araddr,
+    output     [31:0] ICACHE_AXI4_araddr,
     output reg        ICACHE_AXI4_arvalid,
     input             ICACHE_AXI4_arready,
     input  [31:0]     ICACHE_AXI4_rdata,
@@ -139,7 +139,7 @@ assign ICACHE_IFU_rdata = data_reg3;
 assign ICACHE_IFU_raddr = addr_reg3;
 assign ICACHE_IFU_pre_dnpc  = per_pc_reg3;
 assign ICACHE_IFU_valid = data_valid & (~flush);
-
+assign ICACHE_AXI4_araddr = {pc_reg1[31:4], 4'b0} ; 
 assign ICACHE_IFU_stall = icache_stall;
 wire stall;
 assign stall = (state != IDLE);
@@ -293,7 +293,13 @@ always @(posedge clock)begin
 end
 
 always @(posedge clock)begin
-    if(state == IDLE && (!hit_reg1) && reg1_valid) begin
+    if(reset) begin
+        index_buffer <= 0;
+        addr_buffer <= 32'h0;
+        pre_pc_buffer <= 32'h0;
+        tag_buffer <= 0;
+    end 
+    else if(state == IDLE && (!hit_reg1) && reg1_valid) begin
         index_buffer <= index_reg1;
         addr_buffer <= pc_reg1;
         pre_pc_buffer <= reg1_pre_dnpc;
@@ -318,14 +324,13 @@ end
 always @(posedge clock) begin
     if(reset)begin
         ICACHE_AXI4_arvalid <= 0;
-        ICACHE_AXI4_araddr <= 0;
     end
     else if((ICACHE_AXI4_arready && ICACHE_AXI4_arvalid))begin
         ICACHE_AXI4_arvalid <= 1'b0;
     end
     else  if(state == IDLE && (!hit_reg1) && reg1_valid && (!flush) && IFU_AXI4_rready)begin
         ICACHE_AXI4_arvalid <= 1'b1;
-        ICACHE_AXI4_araddr <= {pc_reg1[31:4], 4'b0} ; 
+        
     end
     
 end
