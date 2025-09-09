efe6d6e272ef69818ef67f33cda8b19cfe3e366f
tracer-ysyx <tracer@ysyx.org>
2025-09-09 22:46:49 +0800

    > compile NEMU ysyx_24070003 江贤 Linux jx-virtual-machine 6.5.0-18-generic #18~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb  7 11:40:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:46:49 up  9:30,  1 user,  load average: 1.29, 1.08, 1.01

--- npc/iverilog_test/ysyx_24070003.v
+++ npc/iverilog_test/ysyx_24070003.v
@@ -2988,6 +2988,7 @@ always @(posedge clock) begin
         LSU_AXI4_BREADY <= 1'b0;
 
         LSU_WBU_valid <= 1'b0;
+        LSU_RDATA <= 32'b0;
 
         LSU_AXI_wlast <= 1'b0;
         // lsu_count <=  64'd0;
