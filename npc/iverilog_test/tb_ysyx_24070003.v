81f22ccbe0741b9a436dda352baa064f20ce6344
tracer-ysyx <tracer@ysyx.org>
2025-09-12 18:34:03 +0800

    > compile NEMU ysyx_24070003 江贤 Linux jx-virtual-machine 6.5.0-18-generic #18~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb  7 11:40:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:34:03 up 8 min,  1 user,  load average: 1.49, 0.89, 0.48

--- npc/iverilog_test/tb_ysyx_24070003.v
+++ npc/iverilog_test/tb_ysyx_24070003.v
@@ -177,7 +177,7 @@ always @(posedge clock) begin
         end
         $stop;   // 立即停仿真
     end
-    else if(ifu_count > 'd100000) begin
+    else if(ifu_count > 'd10000) begin
         $display("\n=== TIMEOUT ===");
         $stop;   // 立即停仿真
     end
@@ -221,9 +221,9 @@ ram u_ram (
 //----------------------------------------------------------------------
 // 波形
 //----------------------------------------------------------------------
-// initial begin
-//     $dumpfile("tb.vcd");
-//     $dumpvars(0, tb_ysyx_24070003);
-// end
+initial begin
+    $dumpfile("tb.vcd");
+    $dumpvars(0, tb_ysyx_24070003);
+end
 
 endmodule
