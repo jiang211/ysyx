1ff91d07ec89f32c07d362c5aacb9cd82c810b67
tracer-ysyx <tracer@ysyx.org>
2025-09-13 21:55:48 +0800

    > run npc ysyx_24070003 江贤 Linux jx-virtual-machine 6.5.0-18-generic #18~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb  7 11:40:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:55:48 up  5:21,  1 user,  load average: 1.22, 1.65, 1.87

--- npc/iverilog_test/ysyx_24070003.v
+++ npc/iverilog_test/ysyx_24070003.v
@@ -1581,22 +1581,23 @@ wire upd_hit;
 
 reg [TAG_WIDTH-1:0] btb_tag [2 ** INDEX_WIDTH -1:0][WAY_NUM-1:0];     // 标签存储
 reg [29:0] btb_target [2 ** INDEX_WIDTH -1:0][WAY_NUM-1:0];  // 目标地址存储
-reg [2 ** INDEX_WIDTH -1:0]btb_valid [WAY_NUM-1:0];          // 有效位
+reg btb_valid [2 ** INDEX_WIDTH -1:0][WAY_NUM-1:0];          // 有效位
 
-
+reg [3:0] hit_valid;
+reg [3:0] hit_valid1;
 
 assign cur_index = cur_pc[INDEX_WIDTH+OFFSET_WITH-1:OFFSET_WITH];
 assign cur_tag = cur_pc[ADDR_WIDTH-1:INDEX_WIDTH+OFFSET_WITH];
 assign update_index = update_pc[INDEX_WIDTH+OFFSET_WITH-1:OFFSET_WITH];
 assign update_tag = update_pc[ADDR_WIDTH-1:INDEX_WIDTH+OFFSET_WITH];
 
-assign hit[0] = btb_valid[0][update_index] && (btb_tag[cur_index][0] == cur_tag);
-assign hit[1] = btb_valid[1][update_index] && (btb_tag[cur_index][1] == cur_tag);
+assign hit[0] = btb_valid[cur_index][0] && (btb_tag[cur_index][0] == cur_tag);
+assign hit[1] = btb_valid[cur_index][1] && (btb_tag[cur_index][1] == cur_tag);
 
-assign pred_valid = hit[0] || hit[1];
+assign pred_valid = (hit_valid[cur_index]) ? hit[0] : (hit_valid1[update_index]) ? hit[1] : 0;
 
-assign update_hit[0] = ~btb_valid[0][update_index] ;
-assign update_hit[1] = ~btb_valid[1][update_index] ;
+assign update_hit[0] = ~btb_valid[update_index][0] ;
+assign update_hit[1] = ~btb_valid[update_index][1] ;
 
 
 
@@ -1612,33 +1613,34 @@ always @(posedge clock) begin
     end
 end
 
-
+integer i,j;
 always @(posedge clock) begin
     if(reset)begin
-        btb_valid[0] <= 0;
-        btb_valid[1] <= 0;
+        hit_valid <= 4'b0000;
+        hit_valid1 <= 4'b0000;
     end
     else if(update_valid)begin
         if(update_hit[0]) begin
+            hit_valid[update_index] <= 1;
             btb_tag[update_index][0] <= update_tag;
             btb_target[update_index][0] <= target_pc[31:2];
-            btb_valid[0][update_index] <= 1;
+            btb_valid[update_index][0] <= 1;
         end
         else if(update_hit[1]) begin
+            hit_valid1[update_index] <= 1;
             btb_tag[update_index][1] <= update_tag;
             btb_target[update_index][1] <= target_pc[31:2];
-            btb_valid[1][update_index] <= 1;
+            btb_valid[update_index][1] <= 1;
         end
         else begin
             btb_tag[update_index][update_way] <= update_tag;
             btb_target[update_index][update_way] <= target_pc[31:2];
-            btb_valid[update_way][update_index] <= 1;
+            btb_valid[update_index][update_way] <= 1;
         end
     end
 end
 
 endmodule
-
 module ysyx_24070003_idu(
     input wire [31:0] INSTR,
     input wire [31:0] IFU_IDU_PC,
