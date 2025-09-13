b467aa38b5fc989ea7a69ff2e61c243d812264e7
tracer-ysyx <tracer@ysyx.org>
2025-09-13 20:58:05 +0800

    > run npc ysyx_24070003 江贤 Linux jx-virtual-machine 6.5.0-18-generic #18~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb  7 11:40:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:58:05 up  4:23,  1 user,  load average: 1.12, 0.89, 0.73

--- npc/iverilog_test/ysyx_24070003.v
+++ npc/iverilog_test/ysyx_24070003.v
@@ -1565,7 +1565,7 @@ module ysyx_24070003_btb(
 
 
 localparam WAY_NUM = 2;
-localparam INDEX_WIDTH = 3;
+localparam INDEX_WIDTH = 2;
 localparam ADDR_WIDTH = 32;
 localparam OFFSET_WITH = 2;
 localparam TAG_WIDTH = ADDR_WIDTH - OFFSET_WITH - INDEX_WIDTH;
@@ -1574,14 +1574,14 @@ wire [INDEX_WIDTH-1:0] cur_index;
 wire [TAG_WIDTH-1:0] cur_tag;
 wire [INDEX_WIDTH-1:0] update_index;
 wire [TAG_WIDTH-1:0] update_tag;
-wire hit;
-wire update_hit;
+wire [WAY_NUM-1:0] hit;
+wire [WAY_NUM-1:0] update_hit;
 //wire cur_hit;
 wire upd_hit;
 
-reg [TAG_WIDTH-1:0] btb_tag [2 ** INDEX_WIDTH -1:0];     // 标签存储
-reg [29:0] btb_target [2 ** INDEX_WIDTH -1:0];  // 目标地址存储
-reg [2 ** INDEX_WIDTH -1:0]btb_valid;          // 有效位
+reg [TAG_WIDTH-1:0] btb_tag [2 ** INDEX_WIDTH -1:0][WAY_NUM-1:0];     // 标签存储
+reg [29:0] btb_target [2 ** INDEX_WIDTH -1:0][WAY_NUM-1:0];  // 目标地址存储
+reg [2 ** INDEX_WIDTH -1:0]btb_valid [WAY_NUM-1:0];          // 有效位
 
 
 
@@ -1590,26 +1590,50 @@ assign cur_tag = cur_pc[ADDR_WIDTH-1:INDEX_WIDTH+OFFSET_WITH];
 assign update_index = update_pc[INDEX_WIDTH+OFFSET_WITH-1:OFFSET_WITH];
 assign update_tag = update_pc[ADDR_WIDTH-1:INDEX_WIDTH+OFFSET_WITH];
 
-assign hit = (btb_valid[cur_index]) ? (btb_tag[cur_index] == cur_tag) : 0;
+assign hit[0] = btb_valid[0][update_index] && (btb_tag[cur_index][0] == cur_tag);
+assign hit[1] = btb_valid[1][update_index] && (btb_tag[cur_index][1] == cur_tag);
+
+assign pred_valid = hit[0] || hit[1];
+
+assign update_hit[0] = ~btb_valid[0][update_index] ;
+assign update_hit[1] = ~btb_valid[1][update_index] ;
 
 
 
-assign update_hit = ~btb_valid[update_index] ;
+assign pred_pc = (pred_valid) ? {btb_target[cur_index][hit[1]],2'b00} : cur_pc;
 
-assign pred_valid = hit;
-
-assign pred_pc = (hit) ? {btb_target[cur_index],2'b00} : cur_pc;
+reg update_way;
+always @(posedge clock) begin
+    if(reset)begin 
+        update_way <= 0;
+    end
+    else if(update_valid)begin
+        update_way <= ~update_way;
+    end
+end
 
 
 always @(posedge clock) begin
     if(reset)begin
-        btb_valid <= 8'b0000;
+        btb_valid[0] <= 0;
+        btb_valid[1] <= 0;
     end
     else if(update_valid)begin
-            btb_tag[update_index] <= update_tag;
-            btb_target[update_index] <= target_pc[31:2];
-            btb_valid[update_index] <= 1;
-    
+        if(update_hit[0]) begin
+            btb_tag[update_index][0] <= update_tag;
+            btb_target[update_index][0] <= target_pc[31:2];
+            btb_valid[0][update_index] <= 1;
+        end
+        else if(update_hit[1]) begin
+            btb_tag[update_index][1] <= update_tag;
+            btb_target[update_index][1] <= target_pc[31:2];
+            btb_valid[1][update_index] <= 1;
+        end
+        else begin
+            btb_tag[update_index][update_way] <= update_tag;
+            btb_target[update_index][update_way] <= target_pc[31:2];
+            btb_valid[update_way][update_index] <= 1;
+        end
     end
 end
 
