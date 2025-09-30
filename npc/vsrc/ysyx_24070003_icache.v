5ea506a6f802fb406245641af96a3ee8252d2ba1
tracer-ysyx <tracer@ysyx.org>
2025-09-30 17:20:36 +0800

    > run npc ysyx_24070003 江贤 Linux jx-virtual-machine 6.5.0-18-generic #18~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb  7 11:40:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:20:36 up  2:43,  1 user,  load average: 1.53, 0.98, 0.47

--- npc/vsrc/ysyx_24070003_icache.v
+++ npc/vsrc/ysyx_24070003_icache.v
@@ -119,7 +119,7 @@ assign ICACHE_IFU_raddr = addr_reg3;
 //assign ICACHE_IFU_pre_dnpc  = per_pc_reg3;
 assign ICACHE_IFU_valid = data_valid & (~flush);
 
-assign ICACHE_IFU_stall = (state != IDLE);
+assign ICACHE_IFU_stall = (state != IDLE) && ICACHE_IFU_valid;
 
 
 always @(posedge clock) begin
