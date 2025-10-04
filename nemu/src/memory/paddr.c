1a237d975dba144198e007260f61774dda4e4c5f
221220000-Zhang San <zhangsan@foo.com>
2025-10-04 17:28:18 +0800

    again

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -18,8 +18,8 @@
 #include <device/mmio.h>
 #include <isa.h>
 
-//#define YSYXSOC
-//#ifndef YSYXSOC
+#define YSYXSOC
+#ifndef YSYXSOC
 
 #ifndef CONFIG_TARGET_SHARE  
 #if   defined(CONFIG_PMEM_MALLOC)
@@ -88,7 +88,7 @@ void paddr_write(paddr_t addr, int len, word_t data) {
   IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
   out_of_bound(addr);
 }
-//#endif
+#endif
 #else
 
 
